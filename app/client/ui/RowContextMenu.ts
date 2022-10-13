6726a69332e33a5872b3e57bbd39bd0256fd448e
Arnaud Peich <arnaud.peich@gmail.com>
2022-10-13 17:19:43 +0200

    Localize RowContextMenu

--- app/client/ui/RowContextMenu.ts
+++ app/client/ui/RowContextMenu.ts
@@ -1,6 +1,9 @@
 import { allCommands } from 'app/client/components/commands';
+import {t} from 'app/client/lib/localization';
 import { menuDivider, menuItemCmd } from 'app/client/ui2018/menus';
 import { dom } from 'grainjs';
+
+const translate = (x: string, args?: any): string => t(`RowContextMenu.${x}`, args);
 
 export interface IRowContextMenu {
   disableInsert: boolean;
@@ -16,29 +19,29 @@ export function RowContextMenu({ disableInsert, disableDelete, isViewSorted, num
     // bottom. It could be very confusing for users who might expect the record to stay above or
     // below the active row. Thus in this case we show a single `insert row` command.
     result.push(
-      menuItemCmd(allCommands.insertRecordAfter, 'Insert row',
+      menuItemCmd(allCommands.insertRecordAfter, translate('InsertRow'),
         dom.cls('disabled', disableInsert)),
     );
   } else {
     result.push(
-      menuItemCmd(allCommands.insertRecordBefore, 'Insert row above',
+      menuItemCmd(allCommands.insertRecordBefore, translate('InsertRowAbove'),
         dom.cls('disabled', disableInsert)),
-      menuItemCmd(allCommands.insertRecordAfter, 'Insert row below',
+      menuItemCmd(allCommands.insertRecordAfter, translate('InsertRowBelow'),
         dom.cls('disabled', disableInsert)),
     );
   }
   result.push(
-    menuItemCmd(allCommands.duplicateRows, `Duplicate ${numRows === 1 ? 'row' : 'rows'}`,
+    menuItemCmd(allCommands.duplicateRows, translate('DuplicateRows', { count: numRows }),
       dom.cls('disabled', disableInsert || numRows === 0)),
   );
   result.push(
     menuDivider(),
     // TODO: should show `Delete ${num} rows` when multiple are selected
-    menuItemCmd(allCommands.deleteRecords, 'Delete',
+    menuItemCmd(allCommands.deleteRecords, translate('Delete'),
       dom.cls('disabled', disableDelete)),
   );
   result.push(
     menuDivider(),
-    menuItemCmd(allCommands.copyLink, 'Copy anchor link'));
+    menuItemCmd(allCommands.copyLink, translate('CopyAnchorLink')));
   return result;
 }
