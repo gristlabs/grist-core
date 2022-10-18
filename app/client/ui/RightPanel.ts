134ed03a91fdebe5ab2f50cf16dc59d6afb51a55
Arnaud Peich <arnaud.peich@gmail.com>
2022-10-18 10:24:23 +0200

    Fix localization of field types in RightPanel

--- app/client/ui/RightPanel.ts
+++ app/client/ui/RightPanel.ts
@@ -51,17 +51,17 @@ const TopTab = StringUnion("pageWidget", "field");
 // Represents a subtab of pageWidget in the right side-pane.
 const PageSubTab = StringUnion("widget", "sortAndFilter", "data");
 
-// A map of widget type to the icon and label to use for a field of that widget.
-const fieldTypes = new Map<IWidgetType, {label: string, icon: IconName, pluralLabel: string}>([
-  ['record', {label: translate('Column', { count: 1 }), icon: 'TypeCell', pluralLabel: translate('Column', { count: 2 })}],
-  ['detail', {label: translate('Field', { count: 1 }), icon: 'TypeCell', pluralLabel: translate('Field', { count: 2 })}],
-  ['single', {label: translate('Field', { count: 1 }), icon: 'TypeCell', pluralLabel: translate('Field', { count: 2 })}],
-  ['chart', {label: translate('Series', { count: 1 }), icon: 'ChartLine', pluralLabel: translate('Series', { count: 2 })}],
-  ['custom', {label: translate('Column', { count: 1 }), icon: 'TypeCell', pluralLabel: translate('Column', { count: 2 })}],
-]);
-
 // Returns the icon and label of a type, default to those associate to 'record' type.
 export function getFieldType(widgetType: IWidgetType|null) {
+  // A map of widget type to the icon and label to use for a field of that widget.
+  const fieldTypes = new Map<IWidgetType, {label: string, icon: IconName, pluralLabel: string}>([
+    ['record', {label: translate('Column', { count: 1 }), icon: 'TypeCell', pluralLabel: translate('Column', { count: 2 })}],
+    ['detail', {label: translate('Field', { count: 1 }), icon: 'TypeCell', pluralLabel: translate('Field', { count: 2 })}],
+    ['single', {label: translate('Field', { count: 1 }), icon: 'TypeCell', pluralLabel: translate('Field', { count: 2 })}],
+    ['chart', {label: translate('Series', { count: 1 }), icon: 'ChartLine', pluralLabel: translate('Series', { count: 2 })}],
+    ['custom', {label: translate('Column', { count: 1 }), icon: 'TypeCell', pluralLabel: translate('Column', { count: 2 })}],
+  ]);
+
   return fieldTypes.get(widgetType || 'record') || fieldTypes.get('record')!;
 }
 
