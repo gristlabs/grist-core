f3e5755a2ed3c91d45b4b989de75a6d01a4bd47e
Florent FAYOLLE <florent.fayolle@beta.gouv.fr>
2023-09-07 17:38:19 +0200

    Typo + move selenium test in its own file
    As per Paul comment: https://github.com/gristlabs/grist-core/pull/651#discussion_r1318700964
    
    "If you look in test/deployment you'll see that a selection of tests are imported there, including HomeIntro. These tests are used as a way to remotely check if a deployment of Grist is sane. In grist-core you can see these tests used in test/test_under_docker.sh, where a Grist server is run in a docker container, and then tests are run with the only access to the server being via the network.
    
    Since your test relies on reconfiguring the server, it cannot be used as
    a deployment test. So you'll need to move it to a different file."

--- test/nbrowser/HomeIntro.ts
+++ test/nbrowser/HomeIntro.ts
@@ -41,49 +41,6 @@ describe('HomeIntro', function() {
     it('should allow collapsing examples and remember the state', testExamplesCollapsing);
     it('should show examples workspace with the intro', testExamplesSection);
     it('should render selected Examples workspace specially', testSelectedExamplesPage);
-
-    describe('With GRIST_ANON_PLAYGROUND set to false', function () {
-      gu.withEnvironmentSnapshot({GRIST_ANON_PLAYGROUND: false});
-      it('should show welcome page with signin and signup buttons and "add new" button disabled', async function () {
-        // Sign out
-        const session = await gu.session().personalSite.anon.login();
-
-        // Open doc-menu
-        await session.loadDocMenu('/');
-
-        assert.equal(await driver.find('.test-welcome-title').getText(), 'Welcome to Grist!');
-        assert.match(
-          await driver.find('.test-welcome-text-no-playground').getText(),
-          /Visit our Help Center.*about Grist./
-        );
-
-        // Check the sign-up and sign-in buttons.
-        const getSignUp = async () => await driver.findContent('.test-intro-sign-up', 'Sign up');
-        const getSignIn = async () => await driver.findContent('.test-intro-sign-in', 'Sign in');
-        // Check that these buttons take us to a Grist login page.
-        for (const getButton of [getSignUp, getSignIn]) {
-          const button = await getButton();
-          await button.click();
-          await gu.checkLoginPage();
-          await driver.navigate().back();
-          await gu.waitForDocMenuToLoad();
-        }
-      });
-      it('should not allow creating new documents', async function () {
-        // Sign out
-        const session = await gu.session().personalSite.anon.login();
-
-        // Open doc-menu
-        await session.loadDocMenu('/');
-
-        // Check that add-new button is disabled
-        assert.equal(await driver.find('.test-dm-add-new').matches('[class*=-disabled]'), true);
-
-        // Check that add-new menu is not displayed
-        await driver.find('.test-dm-add-new').doClick();
-        assert.equal(await driver.find('.test-dm-new-doc').isPresent(), false);
-      });
-    });
   });
 
   describe("Logged-in on merged-org", function() {
