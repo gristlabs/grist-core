d16e5cfd1ad9b1dfbd5f309c4f4cb1fa6140e296
Florent FAYOLLE <florent.fayolle@beta.gouv.fr>
2023-11-02 18:11:47 +0100

    Several little fixes + store codeVerifier as Permit in Session

--- app/server/lib/OIDCConfig.ts
+++ app/server/lib/OIDCConfig.ts
@@ -1,6 +1,6 @@
 /**
  * Configuration for OpenID Connect (OIDC), useful for enterprise single-sign-on logins.
- * A good informative overview of SAML is at https://developer.okta.com/blog/2019/10/21/illustrated-guide-to-oauth-and-oidc
+ * A good informative overview of OIDC is at https://developer.okta.com/blog/2019/10/21/illustrated-guide-to-oauth-and-oidc
  * Note:
  *    SP is "Service Provider", in our case, the Grist application.
  *    IdP is the "Identity Provider", somewhere users log into, e.g. Okta or Google Apps.
@@ -36,31 +36,31 @@
 
 import * as express from 'express';
 import {GristLoginSystem, GristServer} from './GristServer';
-import { Client, generators, Issuer } from 'openid-client';
+import { Client, generators, Issuer, UserinfoResponse } from 'openid-client';
 import {Sessions} from './Sessions';
+import log from 'app/server/lib/log';
+import {Permit} from './Permit';
+
 const CALLBACK_URL = '/oauth2/callback';
 const LOGIN_URL = '/oauth2/login';
 
+const LOGIN_ACTION = 'oidc-login';
+const WAIT_IN_MINUTES = 20;
+
 export class OIDCConfig {
   private _client: Client;
-  private _nonce: string;
-  private _codeVerifier: string;
-  private _codeChallenge: string;
 
-  public constructor() {
-    this._nonce = generators.nonce();
-    this._codeVerifier = generators.codeVerifier();
-    this._codeChallenge = generators.codeChallenge(this._codeVerifier);
+  public constructor(private _gristServer: GristServer) {
   }
 
   public async initOIDC(): Promise<void> {
-    if (!process.env.GRIST_OIDC_SP_HOST) { throw new Error('initConfig requires GRIST_OIDC_SP_HOST to be set'); }
-    if (!process.env.GRIST_OIDC_IDP_ISSUER) { throw new Error('initConfig requires GRIST_OIDC_IDP_ISSUER to be set'); }
+    if (!process.env.GRIST_OIDC_SP_HOST) { throw new Error('initOIDC requires GRIST_OIDC_SP_HOST to be set'); }
+    if (!process.env.GRIST_OIDC_IDP_ISSUER) { throw new Error('initOIDC requires GRIST_OIDC_IDP_ISSUER to be set'); }
     if (!process.env.GRIST_OIDC_IDP_CLIENT_ID) {
-      throw new Error('initConfig requires GRIST_OIDC_IDP_CLIENT_ID to be set');
+      throw new Error('initOIDC requires GRIST_OIDC_IDP_CLIENT_ID to be set');
     }
     if (!process.env.GRIST_OIDC_IDP_CLIENT_SECRET) {
-      throw new Error('initConfig requires GRIST_OIDC_IDP_CLIENT_SECRET to be set');
+      throw new Error('initOIDC requires GRIST_OIDC_IDP_CLIENT_SECRET to be set');
     }
     const spHost: string = process.env.GRIST_OIDC_SP_HOST;
     const issuer = await Issuer.discover(process.env.GRIST_OIDC_IDP_ISSUER);
@@ -79,39 +79,50 @@ export class OIDCConfig {
 
   public async handleCallback(sessions: Sessions, req: express.Request, res: express.Response): Promise<void> {
 
-    const params = this._client.callbackParams(req);
-    const { state } = params;
-    const tokenSet = await this._client.callback(
-      new URL(CALLBACK_URL, process.env.GRIST_OIDC_SP_HOST).href,
-      params,
-      { nonce: this._nonce, state, code_verifier: this._codeVerifier }
-    );
-    const userInfo = await this._client.userinfo(tokenSet);
-    const email = userInfo.email;
-    const fname = userInfo.given_name ?? '';
-    const lname = userInfo.family_name ?? '';
-    const profile = {
-      email,
-      name: `${fname} ${lname}`.trim(),
-    };
-    const scopedSession = sessions.getOrCreateSessionFromRequest(req);
-    await scopedSession.operateOnScopedSession(req, async (user) => Object.assign(user, {
-      profile,
-    }));
-    res.redirect('/');
+    try {
+      const params = this._client.callbackParams(req);
+      const { state } = params;
+      if (!state) {
+        throw new Error('Login or logout failed to complete');
+      }
+      const codeVerifier = await this._retrieveCodeVerifierFromPermit(state);
+      const tokenSet = await this._client.callback(
+        new URL(CALLBACK_URL, process.env.GRIST_OIDC_SP_HOST).href,
+        params,
+        { state, code_verifier: codeVerifier }
+      );
+      const userInfo = await this._client.userinfo(tokenSet);
+      const profile = this._makeUserProfileFromUserInfo(userInfo);
+      const scopedSession = sessions.getOrCreateSessionFromRequest(req);
+      await scopedSession.operateOnScopedSession(req, async (user) => Object.assign(user, {
+        profile,
+        accessToken: tokenSet.access_token,
+        refreshToken: tokenSet.refresh_token,
+      }));
+      res.redirect('/');
+    } catch (err) {
+      log.error(`OIDC callback failed: ${err.message}`);
+      res.status(500).send(`Cannot connect to OIDC provider: ${err.message}`);
+    }
   }
 
   public async handleLogin(req: express.Request, res: express.Response): Promise<void> {
-    res.redirect(await this.getLoginRedirectUrl());
+    try {
+      res.redirect(await this.getLoginRedirectUrl(req));
+    } catch (ex) {
+      log.error(`OIDC login failed: ${ex.message}`);
+      res.status(500).send(`Cannot connect to OIDC provider: ${ex.message}`);
+    }
   }
 
-  public async getLoginRedirectUrl(): Promise<string> {
-    const state = generators.state();
+  public async getLoginRedirectUrl(req: express.Request): Promise<string> {
+    const { state, codeVerifier } = await this._generateAndStoreCodeVerifier(req, this._gristServer.getSessions());
+    const codeChallenge = generators.codeChallenge(codeVerifier);
+
     const authUrl = this._client.authorizationUrl({
       scope: process.env.GRIST_OIDC_IDP_SCOPES || 'openid email profile',
-      code_challenge: this._codeChallenge,
+      code_challenge: codeChallenge,
       code_challenge_method: process.env.GRIST_OIDC_IDP_CODE_CHALLENGE_METHOD || 'S256',
-      nonce: this._nonce,
       state,
     });
     return authUrl;
@@ -122,13 +133,51 @@ export class OIDCConfig {
       post_logout_redirect_uri: redirectUrl.href
     });
   }
+
+  private async _generateAndStoreCodeVerifier(req: express.Request, sessions: Sessions) {
+    const permitStore = this._gristServer.getExternalPermitStore();
+    const sessionId = sessions.getSessionIdFromRequest(req);
+    if (!sessionId) { throw new Error('no session available'); }
+    const codeVerifier = generators.codeVerifier();
+    const permit: Permit = {
+      action: LOGIN_ACTION,
+      sessionId,
+      codeVerifier,
+    };
+    const state = await permitStore.setPermit(permit, WAIT_IN_MINUTES * 60 * 1000);
+    return { codeVerifier, state };
+  }
+
+  private async _retrieveCodeVerifierFromPermit(state: string) {
+    const permitStore = this._gristServer.getExternalPermitStore();
+    const permit = await permitStore.getPermit(state);
+    if (!permit || !permit.codeVerifier) {
+      throw new Error('Login or logout is stale');
+    }
+    if (permit.action !== LOGIN_ACTION) {
+      throw new Error(`Unexpected action: "${permit.action}"`);
+    }
+    const { codeVerifier } = permit;
+    await permitStore.removePermit(state);
+    return codeVerifier;
+  }
+
+  private _makeUserProfileFromUserInfo(userInfo: UserinfoResponse) {
+      const email = userInfo.email;
+      const fname = userInfo.given_name ?? '';
+      const lname = userInfo.family_name ?? '';
+      return {
+        email,
+        name: `${fname} ${lname}`.trim(),
+      };
+  }
 }
 
 export async function getOIDCLoginSystem(): Promise<GristLoginSystem|undefined> {
   if (!process.env.GRIST_OIDC_SP_HOST) { return undefined; }
   return {
     async getMiddleware(gristServer: GristServer) {
-      const config = new OIDCConfig();
+      const config = new OIDCConfig(gristServer);
       await config.initOIDC();
       return {
         getLoginRedirectUrl: config.getLoginRedirectUrl.bind(config),
--- app/server/lib/Permit.ts
+++ app/server/lib/Permit.ts
@@ -32,6 +32,7 @@ export interface Permit {
   sessionId?: string;   // A particular session.
   url?: string;         // A particular url.
   action?: string;      // A string denoting what kind of action the permit applies to.
+  codeVerifier?: string;
 }
 
 /* A store of permits */
