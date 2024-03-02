1be3e5db9605a5a5cfdddd5497757ab092605e5e
Thomas Weaver <105765011+thomasweaver627@users.noreply.github.com>
2024-03-02 16:02:16 -0500

    fixes #852 PR

--- app/client/ui/DateRangeOptions.ts
+++ app/client/ui/DateRangeOptions.ts
@@ -10,35 +10,35 @@ export interface IDateRangeOption {
 }
 
 export const DateRangeOptions: IDateRangeOption[] = [{
-  label: 'Today',
+  label: t('Today'),
   min: CURRENT_DATE,
   max: CURRENT_DATE,
 }, {
-  label: 'Last 7 days',
+  label: t('Last 7 days'),
   min: [{quantity: -7, unit: 'day'}],
   max: [{quantity: -1, unit: 'day'}],
 }, {
-  label: 'Next 7 days',
+  label: t('Next 7 days'),
   min: [{quantity: 1, unit: 'day'}],
   max: [{quantity: 7, unit: 'day'}],
 }, {
-  label: 'Last Week',
+  label: t('Last Week'),
   min: [{quantity: -1, unit: 'week'}],
   max: [{quantity: -1, unit: 'week', endOf: true}],
 }, {
-  label: 'Last 30 days',
+  label: t('Last 30 days'),
   min: [{quantity: -30, unit: 'day'}],
   max: [{quantity: -1, unit: 'day'}],
 }, {
-  label: 'This week',
+  label: t('This week'),
   min: [{quantity: 0, unit: 'week'}],
   max: [{quantity: 0, unit: 'week', endOf: true}],
 }, {
-  label: 'This month',
+  label: t('This month'),
   min: [{quantity: 0, unit: 'month'}],
   max: [{quantity: 0, unit: 'month', endOf: true}],
 }, {
-  label: 'This year',
+  label: t('This year'),
   min: [{quantity: 0, unit: 'year'}],
   max: [{quantity: 0, unit: 'year', endOf: true}],
 }];
