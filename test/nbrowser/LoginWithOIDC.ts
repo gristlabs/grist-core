bb3874a234cfe15617e35de298129111c83d3dc7
fflorent <florent.git@zeteo.me>
2024-03-06 14:28:29 +0100

    WIP: LoginWithOIDC

---
+++ test/nbrowser/LoginWithOIDC.ts
@@ -0,0 +1,21 @@
+// import {assert, driver} from 'mocha-webdriver';
+// import * as gu from 'test/nbrowser/gristUtils';
+// import {setupTestSuite} from 'test/nbrowser/testUtils';
+// import express from 'express';
+
+// describe('LoginWithOIDC', function () {
+//   this.timeout(60000);
+//   setupTestSuite();
+//   before(async () => {
+//     const app = express();
+//     app.get('/.well-known/openid-configuration', (req, res) => {
+//       res.json({
+//       });
+//     });
+//     app.listen(gu.
+//   });
+//   gu.withEnvironmentSnapshot({
+//     'GRIST_OIDC_IDP_ISSUER': 'https://accounts.google.com',
+//   })
+// });
+
