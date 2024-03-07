a4fb828eef72c013f0bf3094fa190ae87628cf6c
Thomas Weaver <105765011+thomasweaver627@users.noreply.github.com>
2024-03-07 17:49:52 -0500

    Requested changes for iss #852

--- app/client/ui/DateRangeOptions.ts
+++ app/client/ui/DateRangeOptions.ts
@@ -9,36 +9,47 @@ export interface IDateRangeOption {
   max: IRelativeDateSpec;
 }
 
-export const DateRangeOptions: IDateRangeOption[] = [{
-  label: t('Today'),
-  min: CURRENT_DATE,
-  max: CURRENT_DATE,
-}, {
-  label: t('Last 7 days'),
-  min: [{quantity: -7, unit: 'day'}],
-  max: [{quantity: -1, unit: 'day'}],
-}, {
-  label: t('Next 7 days'),
-  min: [{quantity: 1, unit: 'day'}],
-  max: [{quantity: 7, unit: 'day'}],
-}, {
-  label: t('Last Week'),
-  min: [{quantity: -1, unit: 'week'}],
-  max: [{quantity: -1, unit: 'week', endOf: true}],
-}, {
-  label: t('Last 30 days'),
-  min: [{quantity: -30, unit: 'day'}],
-  max: [{quantity: -1, unit: 'day'}],
-}, {
-  label: t('This week'),
-  min: [{quantity: 0, unit: 'week'}],
-  max: [{quantity: 0, unit: 'week', endOf: true}],
-}, {
-  label: t('This month'),
-  min: [{quantity: 0, unit: 'month'}],
-  max: [{quantity: 0, unit: 'month', endOf: true}],
-}, {
-  label: t('This year'),
-  min: [{quantity: 0, unit: 'year'}],
-  max: [{quantity: 0, unit: 'year', endOf: true}],
-}];
+export function getDateRangeOptions(): IDateRangeOption[] {
+  return [
+    {
+      label: t('Today'),
+      min: CURRENT_DATE,
+      max: CURRENT_DATE,
+    },
+    {
+      label: t('Last 7 days'),
+      min: [{quantity: -7, unit: 'day'}],
+      max: [{quantity: -1, unit: 'day'}],
+    },
+    {
+      label: t('Next 7 days'),
+      min: [{quantity: 1, unit: 'day'}],
+      max: [{quantity: 7, unit: 'day'}],
+    },
+    {
+      label: t('Last Week'),
+      min: [{quantity: -1, unit: 'week'}],
+      max: [{quantity: -1, unit: 'week', endOf: true}],
+    },
+    {
+      label: t('Last 30 days'),
+      min: [{quantity: -30, unit: 'day'}],
+      max: [{quantity: -1, unit: 'day'}],
+    },
+    {
+      label: t('This week'),
+      min: [{quantity: 0, unit: 'week'}],
+      max: [{quantity: 0, unit: 'week', endOf: true}],
+    },
+    {
+      label: t('This month'),
+      min: [{quantity: 0, unit: 'month'}],
+      max: [{quantity: 0, unit: 'month', endOf: true}],
+    },
+    {
+      label: t('This year'),
+      min: [{quantity: 0, unit: 'year'}],
+      max: [{quantity: 0, unit: 'year', endOf: true}],
+    },
+  ];
+}
