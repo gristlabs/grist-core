41806e39897c6f4af90ab8d381ae40c200582933
uowis <mail@example.com>
2024-05-11 18:08:57 +0200

    add missing semicolon

--- app/client/ui/errorPages.ts
+++ app/client/ui/errorPages.ts
@@ -98,7 +98,7 @@ export function createMfaNotEnabledErrorPage(appModel: AppModel) {
     cssButtonWrap(bigBasicButtonLink(
       t("Try again"), {href: getSignupUrl({ nextUrl: searchParams.get("next") || "" })}, testId('error-signin')
     ))
-  ])
+  ]);
 }
 
 /**
