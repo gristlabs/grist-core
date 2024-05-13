c1070fa059e06739b59678d0f2d3dcbcafa715f8
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2024-05-13 17:36:22 -0400

    missing await in test

--- test/nbrowser/Boot.ts
+++ test/nbrowser/Boot.ts
@@ -18,7 +18,7 @@ describe('Boot', function() {
   async function hasPrompt() {
     // There is some glitchiness to when the text appears.
     gu.waitToPass(async () => {
-      assert.include(
+      await assert.include(
         await driver.findContentWait('pre', /GRIST_BOOT_KEY/, 2000).getText(),
         'GRIST_BOOT_KEY=secret');
     }, 3000);
