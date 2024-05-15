e55fa0869bc2cadff7cc015033825de2af32b2eb
Grégoire Cutzach <gregoire@cutzach.com>
2024-05-15 18:28:20 +0200

    fix: bad indentation in app/client/ui/HomeLeftPane.ts
    Co-authored-by: Jonathan Perret <j-github@jonathanperret.net>

--- app/client/ui/HomeLeftPane.ts
+++ app/client/ui/HomeLeftPane.ts
@@ -146,7 +146,7 @@ export function createHomeLeftPane(leftPanelOpen: Observable<boolean>, home: Hom
           ) : null
         ),
         createHelpTools(home.app),
-       (commonUrls.termsOfService ?
+        (commonUrls.termsOfService ?
           cssPageEntry(
             cssPageLink(cssPageIcon('Memo'), cssLinkText(t("Terms of service")),
               { href: commonUrls.termsOfService, target: '_blank' },
