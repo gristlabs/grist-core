bc4a8a90951f9f448032073c584971f3d89bd862
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2024-05-16 14:16:20 -0400

    fix message for non-admins

--- app/client/ui/AdminPanel.ts
+++ app/client/ui/AdminPanel.ts
@@ -101,7 +101,13 @@ export class AdminPanel extends Disposable {
     return dom.create(AdminSection, t('Administrator Panel Unavailable'), [
       dom('p', t(`You do not have access to the administrator panel.
 Please log in as an administrator.`)),
-      dom('p', t(`Or, as a fallback, you can set: {{bootKey}} in the environment and visit: {{url}}`), {bootKey: dom('pre', 'GRIST_BOOT_KEY=secret'), url: dom('pre', `/admin?key=secret`)}),
+      dom(
+        'p',
+        t(`Or, as a fallback, you can set: {{bootKey}} in the environment and visit: {{url}}`, {
+          bootKey: dom('pre', 'GRIST_BOOT_KEY=secret'),
+          url: dom('pre', `/admin?key=secret`)
+        }),
+      ),
     ]);
   }
 
