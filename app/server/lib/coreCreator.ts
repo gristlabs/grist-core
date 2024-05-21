c575090594ddbac72769c1ab1801cb86ca3af910
Spoffy <contact@spoffy.net>
2024-05-21 22:47:51 +0100

    Moves ICreate and GristLoginSystem code to server/lib.
    Enables code in ext/ to be able to access it (e.g for proxying / interception).
    
    Adds getCreate() to enable future refactoring of `const create` away from being a global singleston constant.

---
+++ app/server/lib/coreCreator.ts
@@ -0,0 +1,22 @@
+import { checkMinIOBucket, checkMinIOExternalStorage,
+         configureMinIOExternalStorage } from 'app/server/lib/configureMinIOExternalStorage';
+import { makeSimpleCreator } from 'app/server/lib/ICreate';
+import { Telemetry } from 'app/server/lib/Telemetry';
+
+export const makeCoreCreator = () => makeSimpleCreator({
+  deploymentType: 'core',
+  // This can and should be overridden by GRIST_SESSION_SECRET
+  // (or generated randomly per install, like grist-omnibus does).
+  sessionSecret: 'Phoo2ag1jaiz6Moo2Iese2xoaphahbai3oNg7diemohlah0ohtae9iengafieS2Hae7quungoCi9iaPh',
+  storage: [
+    {
+      name: 'minio',
+      check: () => checkMinIOExternalStorage() !== undefined,
+      checkBackend: () => checkMinIOBucket(),
+      create: configureMinIOExternalStorage,
+    },
+  ],
+  telemetry: {
+    create: (dbManager, gristServer) => new Telemetry(dbManager, gristServer),
+  }
+});
