c87999c53d87545191718254d1d82440528ae9f4
Spoffy <contact@spoffy.net>
2024-06-11 23:12:05 +0100

    Splits directory checks into read and write

--- sandbox/docker_entrypoint.sh
+++ sandbox/docker_entrypoint.sh
@@ -3,7 +3,9 @@ set -Eeuo pipefail
 
 # Runs the command provided as arguments, but attempts to configure permissions first.
 
-important_dirs=("/grist" "/persist")
+important_read_dirs=("/grist" "/persist")
+important_write_dirs=("/persist")
+important_dirs=( "${important_read_dirs[@]}" "${important_write_dirs[@]}" )
 current_user_id=$(id -u)
 
 # We want to avoid running Grist as root if possible.
@@ -14,7 +16,7 @@ if [[ $current_user_id == 0 ]]; then
 
   for dir in "${important_dirs[@]}"; do
     # Make sure the target user owns everything that Grist needs read/write access to.
-    find "$dir" ! -user $target_user -exec chown $target_user "{}" +
+    find "$dir" ! -user "$target_user" -exec chown "$target_user" "{}" +
   done
 
   # Restart as the target user, replacing the current process (replacement is needed for security).
@@ -24,9 +26,15 @@ fi
 
 # Validate that this user has access to the top level of each important directory.
 # There might be a benefit to testing an individual file in there,
-for dir in "${important_dirs[@]}"; do
+for dir in "${important_read_dirs[@]}"; do
+  if ! { test -r "$dir" ;} ; then
+    echo "Invalid permissions, cannot read '$dir'. Aborting." >&2
+    exit 1
+  fi
+done
+for dir in "${important_write_dirs[@]}"; do
   if ! { test -r "$dir" && test -w "$dir" ;} ; then
-    echo "Invalid permissions, cannot read/write '$dir'. Aborting." >&2
+    echo "Invalid permissions, cannot write '$dir'. Aborting." >&2
     exit 1
   fi
 done
