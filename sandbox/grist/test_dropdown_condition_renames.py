d9d510049652ee4cc02dea46cdfce1c31cdacd82
Leslie H. <che27@illinois.edu>
2024-06-14 18:04:04 -0500

    Add comments to new test case

--- sandbox/grist/test_dropdown_condition_renames.py
+++ sandbox/grist/test_dropdown_condition_renames.py
@@ -14,61 +14,71 @@ class TestDCRenames(test_engine.EngineTestCase):
     self.load_sample(testsamples.sample_students)
 
     self.engine.apply_user_actions([useractions.from_repr(ua) for ua in (
-      ['UpdateRecord', '_grist_Tables_column', 12, {
-        'widgetOptions': json.dumps({
-          'dropdownCondition': {
-            'text': '"New" in choice.city and $name == rec.name',
+      # Column #12 is "address" in table "Schools". We add a dropdown condition formula to it.
+      ["UpdateRecord", "_grist_Tables_column", 12, {
+        "widgetOptions": json.dumps({
+          "dropdownCondition": {
+            "text": "'New' in choice.city and $name == rec.name",
           }
         }),
       }],
     )])
 
-    self.assertTableData('_grist_Tables_column', cols="subset", rows="subset", data=[
-      ['id',  'type',         'widgetOptions'],
-      [12,    'Ref:Address',  json.dumps({
-        'dropdownCondition': {
-          'text': '"New" in choice.city and $name == rec.name',
-          'parsed': json.dumps(["And", ["In", ["Const", "New"], ["Attr", ["Name", "choice"], "city"]],
+    # This is what we"ll have at the beginning, for later tests to refer to.
+    self.assertTableData("_grist_Tables_column", cols="subset", rows="subset", data=[
+      ["id",  "type",         "widgetOptions"],
+      [12,    "Ref:Address",  json.dumps({
+        "dropdownCondition": {
+          "text": "'New' in choice.city and $name == rec.name",
+          # The UpdateRecord user action should trigger an auto parse.
+          # "parsed" is stored as dumped JSON, so we need to explicitly dump it here as well.
+          "parsed": json.dumps(["And", ["In", ["Const", "New"], ["Attr", ["Name", "choice"], "city"]],
                                 ["Eq", ["Attr", ["Name", "rec"], "name"], ["Attr", ["Name", "rec"], "name"]]])
         }
       })],
     ])
 
   def test_referred_column_renames(self):
-    self.apply_user_action(['RenameColumn', 'Address', 'city', 'area'])
-    self.assertTableData('_grist_Tables_column', cols="subset", rows="subset", data=[
-      ['id',  'type',         'widgetOptions'],
-      [12,    'Ref:Address',  json.dumps({
-        'dropdownCondition': {
-          'text': '"New" in choice.area and $name == rec.name',
-          'parsed': json.dumps(["And", ["In", ["Const", "New"], ["Attr", ["Name", "choice"], "area"]],
+    # Rename the column "city" in table "Address" to "area". Schools.address refers to it.
+    self.apply_user_action(["RenameColumn", "Address", "city", "area"])
+    # Now choice.city should become choice.area. This should also be reflected in the parsed formula.
+    self.assertTableData("_grist_Tables_column", cols="subset", rows="subset", data=[
+      ["id",  "type",         "widgetOptions"],
+      [12,    "Ref:Address",  json.dumps({
+        "dropdownCondition": {
+          "text": "'New' in choice.area and $name == rec.name",
+          "parsed": json.dumps(["And", ["In", ["Const", "New"], ["Attr", ["Name", "choice"], "area"]],
                                 ["Eq", ["Attr", ["Name", "rec"], "name"], ["Attr", ["Name", "rec"], "name"]]])
         }
       })],
     ])
 
   def test_record_column_renames(self):
-    self.apply_user_action(['RenameColumn', 'Schools', 'name', 'identifier'])
-    self.assertTableData('_grist_Tables_column', cols="subset", rows="subset", data=[
-      ['id',  'type',         'widgetOptions'],
-      [12,    'Ref:Address',  json.dumps({
-        'dropdownCondition': {
-          'text': '"New" in choice.city and $identifier == rec.identifier',
-          'parsed': json.dumps(["And", ["In", ["Const", "New"], ["Attr", ["Name", "choice"], "city"]],
+    # Rename the column "name" in table "Schools" to "identifier". Schools.address refers to it in two ways -
+    # the dollar sign and "rec.".
+    self.apply_user_action(["RenameColumn", "Schools", "name", "identifier"])
+    # Now "$name" should become "$identifier" while "rec.name" should become "rec.identifier".
+    self.assertTableData("_grist_Tables_column", cols="subset", rows="subset", data=[
+      ["id",  "type",         "widgetOptions"],
+      [12,    "Ref:Address",  json.dumps({
+        "dropdownCondition": {
+          "text": "'New' in choice.city and $identifier == rec.identifier",
+          "parsed": json.dumps(["And", ["In", ["Const", "New"], ["Attr", ["Name", "choice"], "city"]],
                                 ["Eq", ["Attr", ["Name", "rec"], "identifier"], ["Attr", ["Name", "rec"], "identifier"]]])
         }
       })],
     ])
 
   def test_multiple_renames(self):
-    self.apply_user_action(['RenameColumn', 'Address', 'city', 'area'])
-    self.apply_user_action(['RenameColumn', 'Schools', 'name', 'identifier'])
-    self.assertTableData('_grist_Tables_column', cols="subset", rows="subset", data=[
-      ['id',  'type',         'widgetOptions'],
-      [12,    'Ref:Address',  json.dumps({
-        'dropdownCondition': {
-          'text': '"New" in choice.area and $identifier == rec.identifier',
-          'parsed': json.dumps(["And", ["In", ["Const", "New"], ["Attr", ["Name", "choice"], "area"]],
+    # Put all renames together.
+    self.apply_user_action(["RenameColumn", "Address", "city", "area"])
+    self.apply_user_action(["RenameColumn", "Schools", "name", "identifier"])
+    self.assertTableData("_grist_Tables_column", cols="subset", rows="subset", data=[
+      ["id",  "type",         "widgetOptions"],
+      [12,    "Ref:Address",  json.dumps({
+        "dropdownCondition": {
+          "text": "'New' in choice.area and $identifier == rec.identifier",
+          "parsed": json.dumps(["And", ["In", ["Const", "New"], ["Attr", ["Name", "choice"], "area"]],
                                 ["Eq", ["Attr", ["Name", "rec"], "identifier"], ["Attr", ["Name", "rec"], "identifier"]]])
         }
       })],
