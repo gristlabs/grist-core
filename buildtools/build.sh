c00dd774c3ea59f7883e794ae997870b3f29f91a
Jordi Gutiérrez Hermoso <jordigh@octave.org>
2024-06-19 12:17:48 -0400

    build.sh: add some diagnostic output
    As I was testing, I found it useful to see when I was using the ext/
    directory or not.

--- buildtools/build.sh
+++ buildtools/build.sh
@@ -5,7 +5,11 @@ set -e
 PROJECT=""
 if [[ -e ext/app ]]; then
   PROJECT="tsconfig-ext.json"
+  echo "Using extra app directory"
+else
+  echo "No extra app directory found"
 fi
+
 WEBPACK_CONFIG=buildtools/webpack.config.js
 if [[ -e ext/buildtools/webpack.config.js ]]; then
   # Allow webpack config file to be replaced (useful
