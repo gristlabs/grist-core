1374d83ae1a9e429bda55a88e41c47d3423eb5c1
Jordi Gutiérrez Hermoso <jordigh@octave.org>
2024-06-19 12:18:34 -0400

    checkout-ext-directory: new helper script
    This is just a helper script to get the ext directory of other grist
    repos, currently intended for grist-ee.

---
+++ buildtools/checkout-ext-directory.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# This checks out the ext/ directory from the extra repo (e.g.
+# grist-ee or grist-desktop) depending on the supplied repo name.
+
+set -e
+
+repo=$1
+dir=$(dirname $0)
+ref=$(cat $dir/.$repo-version)
+
+git clone --branch $ref --depth 1 --filter=tree:0 "https://github.com/gristlabs/$repo"
+pushd $repo
+git sparse-checkout set ext
+git checkout
+popd
+mv $repo/ext .
+rm -rf $repo
