61421e82510f924010e356ab6b36a8b7bad3afa7
CamilleLegeron <camille@telescoop.fr>
2024-07-01 15:13:39 +0200

    Create user last connection datetime (#935)
    Each time the a Grist page is reload the `last_connection_at` of the user is updated
    
    resolve [#924](https://github.com/gristlabs/grist-core/issues/924)

--- app/server/lib/requestUtils.ts
+++ app/server/lib/requestUtils.ts
@@ -21,8 +21,8 @@ export const TEST_HTTPS_OFFSET = process.env.GRIST_TEST_HTTPS_OFFSET ?
 
 // Database fields that we permit in entities but don't want to cross the api.
 const INTERNAL_FIELDS = new Set([
-  'apiKey', 'billingAccountId', 'firstLoginAt', 'filteredOut', 'ownerId', 'gracePeriodStart', 'stripeCustomerId',
-  'stripeSubscriptionId', 'stripeProductId', 'userId', 'isFirstTimeUser', 'allowGoogleLogin',
+  'apiKey', 'billingAccountId', 'firstLoginAt', 'lastConnectionAt', 'filteredOut', 'ownerId', 'gracePeriodStart',
+  'stripeCustomerId', 'stripeSubscriptionId', 'stripeProductId', 'userId', 'isFirstTimeUser', 'allowGoogleLogin',
   'authSubject', 'usage', 'createdBy'
 ]);
 
