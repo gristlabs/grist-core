1c71b4183bbb21d4072f3829cf6660bed04120af
Spoffy <contact@spoffy.net>
2024-08-05 19:26:46 +0100

    Makes docker-compose examples ignore persistent values and secrets

--- docker-compose-examples/grist-traefik-oidc-auth/generateSecureSecrets.sh
+++ docker-compose-examples/grist-traefik-oidc-auth/generateSecureSecrets.sh
@@ -2,8 +2,11 @@
 
 SCRIPT_DIR=$(dirname $0)
 
-mkdir -p "$SCRIPT_DIR/secrets"
+# Copy over template files to final locations
+cp -R "$SCRIPT_DIR/secrets_template" "$SCRIPT_DIR/secrets"
+cp "$SCRIPT_DIR/env-template" "$SCRIPT_DIR/.env"
 
+# Parses an Aurelia generated secret for the value
 function getSecret {
   cut -d ":" -f 2 <<< "$1" | tr -d '[:blank:]'
 }
@@ -20,6 +23,7 @@ generateSecureString 128 > "$SCRIPT_DIR/secrets/STORAGE_ENCRYPTION_KEY"
 # Generates the OIDC secret key for the Grist client
 CLIENT_SECRET_OUTPUT="$(docker run authelia/authelia:4 authelia crypto hash generate pbkdf2 --variant sha512 --random --random.length 72 --random.charset rfc3986)"
 CLIENT_SECRET=$(getSecret "$(grep 'Password' <<< $CLIENT_SECRET_OUTPUT)")
+sed -i "/GRIST_CLIENT_SECRET=$/d" "$SCRIPT_DIR/.env"
 echo "GRIST_CLIENT_SECRET=$CLIENT_SECRET" >> "$SCRIPT_DIR/.env"
 getSecret "$(grep 'Digest' <<< $CLIENT_SECRET_OUTPUT)" >> "$SCRIPT_DIR/secrets/GRIST_CLIENT_SECRET_DIGEST"
 
