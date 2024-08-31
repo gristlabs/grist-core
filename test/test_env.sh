90c08bc6ed68ab9626625512de44db7fe6b3aaff
George Gevoian <george@gevoian.com>
2024-08-30 22:28:33 -0400

    Pin Selenium to Chrome 127

---
+++ test/test_env.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export GRIST_SESSION_COOKIE="grist_test_cookie"
+export GRIST_TEST_LOGIN="1"
+export LANGUAGE="en_US"
+export SE_BROWSER="chrome"
+export SE_BROWSER_VERSION="127"
+export SE_DRIVER="chrome-driver"
+export SE_DRIVER_VERSION="127.0.6533.119"
+export TEST_CLEAN_DATABASE="true"
+export TEST_SUPPORT_API_KEY="api_key_for_support"
+
+exec "$@"
