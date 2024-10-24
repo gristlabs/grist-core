12cfb98d6591f01ebe1bfce07f532dcc0c86f6a6
Grégoire Cutzach <gregoire@cutzach.com>
2024-10-24 09:34:53 +0200

    chore: ADD LoginsEmailsIndex migration to tests

--- app/gen-server/migration/1729754662550-LoginsEmailIndex.ts
+++ app/gen-server/migration/1729754662550-LoginsEmailIndex.ts
@@ -1,6 +1,6 @@
 import {MigrationInterface, QueryRunner, TableIndex} from "typeorm";
 
-export class LoginsEmailsIndex1727689195356 implements MigrationInterface {
+export class LoginsEmailsIndex1729754662550 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.createIndex("logins", new TableIndex({
