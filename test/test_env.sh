3e4699f19abd89244f8b693b3c3cd0ff5476114f
George Gevoian <george@gevoian.com>
2024-10-29 15:32:35 -0700

    Bump Chrome for Testing version  to 130

--- test/test_env.sh
+++ test/test_env.sh
@@ -3,9 +3,9 @@
 export GRIST_SESSION_COOKIE="grist_test_cookie"
 export LANGUAGE="en_US"
 export SE_BROWSER="chrome"
-export SE_BROWSER_VERSION="127"
+export SE_BROWSER_VERSION="130"
 export SE_DRIVER="chrome-driver"
-export SE_DRIVER_VERSION="127.0.6533.119"
+export SE_DRIVER_VERSION="130.0.6723.91"
 export TEST_CLEAN_DATABASE="true"
 export TEST_SUPPORT_API_KEY="api_key_for_support"
 
