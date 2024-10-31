bf337d1f4f7cd2fed03a0142a484f6a788e8f461
Dmitry S <dsagal+git@gmail.com>
2024-10-31 02:02:40 -0400

    (core) Fix flaky ViewLayoutCollapse and Smoke tests
    Summary:
    For ViewLayoutCollapse: use a smaller version of the overly large Investment Research doc
    For Smoke test: hit Enter to open a cell, as a more reliable behavior.
    
    Test Plan: Updated test for the reduced version of the sample doc.
    
    Reviewers: georgegevoian
    
    Reviewed By: georgegevoian
    
    Differential Revision: https://phab.getgrist.com/D4392

--- test/nbrowser/ViewLayoutCollapse.ts
+++ test/nbrowser/ViewLayoutCollapse.ts
@@ -95,7 +95,7 @@ describe("ViewLayoutCollapse", function() {
   });
 
   it('fix: custom widget should restart when added back after collapsing', async function() {
-    await session.tempDoc(cleanup, 'Investment Research.grist');
+    await session.tempDoc(cleanup, 'Investment Research (smaller).grist');
     await gu.openPage("Overview");
 
     const revert = await gu.begin();
@@ -252,8 +252,7 @@ describe("ViewLayoutCollapse", function() {
     await gu.checkForErrors();
 
     assert.equal(await gu.getActiveSectionTitle(), INVESTMENTS);
-    // Make sure we are in 1column 9th row.
-    assert.deepEqual(await gu.getCursorPosition(), {rowNum: 9, col: 0});
+    assert.deepEqual(await gu.getCursorPosition(), {rowNum: 8, col: 0});
 
     // Hide companies chart, and search for mobile (should show no results).
     await collapseByMenu(COMPANIES_CHART);
@@ -269,10 +268,10 @@ describe("ViewLayoutCollapse", function() {
     // Now search for web.
     await gu.closeSearch();
     await gu.search('web');
-    assert.deepEqual(await gu.getCursorPosition(), {rowNum: 5, col: 0});
+    assert.deepEqual(await gu.getCursorPosition(), {rowNum: 2, col: 0});
 
     // Recreate document (can't undo).
-    await session.tempDoc(cleanup, 'Investment Research.grist');
+    await session.tempDoc(cleanup, 'Investment Research (smaller).grist');
   });
 
 
@@ -397,7 +396,7 @@ describe("ViewLayoutCollapse", function() {
     assert.equal(cursor.col, 1);
     assert.equal(await gu.getActiveCell().getText(), 'angel');
     assert.equal(await gu.getActiveSectionTitle(), 'INVESTMENTS');
-    assert.match(await driver.getCurrentUrl(), /Investment-Research\/p\/1$/);
+    assert.match(await driver.getCurrentUrl(), /Investment-Research-smaller\/p\/1$/);
     await revert();
   });
 
