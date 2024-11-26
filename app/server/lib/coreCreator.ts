d4082b76e2cfa4bd51bcb1c1b84748f8ebe075d7
George Gevoian <george@gevoian.com>
2024-11-25 19:12:25 -0500

    (core) Reorganize audit logging code
    Summary:
    Moves around some audit logging-related code, and updates grist-ee
    banners to include contact links.
    
    Test Plan: Existing tests.
    
    Reviewers: paulfitz
    
    Reviewed By: paulfitz
    
    Subscribers: paulfitz
    
    Differential Revision: https://phab.getgrist.com/D4407

--- app/server/lib/coreCreator.ts
+++ app/server/lib/coreCreator.ts
@@ -1,6 +1,5 @@
 import { checkMinIOBucket, checkMinIOExternalStorage,
          configureMinIOExternalStorage } from 'app/server/lib/configureMinIOExternalStorage';
-import { configureCoreAuditLogger } from 'app/server/lib/configureCoreAuditLogger';
 import { makeSimpleCreator } from 'app/server/lib/ICreate';
 import { Telemetry } from 'app/server/lib/Telemetry';
 
@@ -14,9 +13,6 @@ export const makeCoreCreator = () => makeSimpleCreator({
       create: configureMinIOExternalStorage,
     },
   ],
-  auditLogger: {
-    create: configureCoreAuditLogger,
-  },
   telemetry: {
     create: (dbManager, gristServer) => new Telemetry(dbManager, gristServer),
   }
