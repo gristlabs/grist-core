de744527f4113420287b3b47e684bbc506e003c7
George Gevoian <george@gevoian.com>
2025-01-23 16:42:27 -0800

    Remove "--no-sandbox" arg

--- test/test_env.sh
+++ test/test_env.sh
@@ -2,7 +2,6 @@
 
 export GRIST_SESSION_COOKIE="grist_test_cookie"
 export LANGUAGE="en_US"
-export MOCHA_WEBDRIVER_ARGS="--no-sandbox"
 export TEST_CLEAN_DATABASE="true"
 export TEST_SUPPORT_API_KEY="api_key_for_support"
 
