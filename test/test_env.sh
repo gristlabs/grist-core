0c42a357c0373f44cf0297ca5a5faf344b9016a6
George Gevoian <george@gevoian.com>
2025-01-23 16:36:29 -0800

    Install Chrome manually in workflows

--- test/test_env.sh
+++ test/test_env.sh
@@ -2,10 +2,7 @@
 
 export GRIST_SESSION_COOKIE="grist_test_cookie"
 export LANGUAGE="en_US"
-export SE_BROWSER="chrome"
-export SE_BROWSER_VERSION="131"
-export SE_DRIVER="chrome-driver"
-export SE_DRIVER_VERSION="131.0.6778.69"
+export MOCHA_WEBDRIVER_ARGS="--no-sandbox"
 export TEST_CLEAN_DATABASE="true"
 export TEST_SUPPORT_API_KEY="api_key_for_support"
 
