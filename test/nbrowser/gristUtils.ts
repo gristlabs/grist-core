bf5ea0f8f2b5106f719ae372043bde83311bdd62
Emmanuel Pelletier <manu@habite.la>
2025-02-18 16:11:02 +0100

    (kb) update tests after keyboard region switching work
    - sometimes when we want to return on the "app focus", it won't mean the
    clipboard element anymore
    - normalize all the waitAppFocus() and waitAppFocus(true) calls to find
    them quicker in the codebase when doing searches

--- test/nbrowser/gristUtils.ts
+++ test/nbrowser/gristUtils.ts
@@ -803,7 +803,6 @@ export async function enterCell(...keys: string[]) {
   }
   await driver.sendKeys(...keys);
   await waitForServer();    // Wait for the value to be saved
-  await waitAppFocus();     // Wait for the cell editor to be closed (maybe unnecessary)
 }
 
 /**
@@ -1171,7 +1170,6 @@ export async function docMenuImport(filePath: string) {
   });
 }
 
-
 /**
  * Wait for the focus to return to the main application, i.e. the special .copypaste element that
  * normally has it (as opposed to an open cell editor, or a focus in some input or menu). Specify
@@ -1179,6 +1177,13 @@ export async function docMenuImport(filePath: string) {
  */
 export async function waitAppFocus(yesNo: boolean = true): Promise<void> {
   await driver.wait(async () => (await driver.find('.copypaste').hasFocus()) === yesNo, 5000);
+}
+
+/**
+ * Wait for the focus to be on the first element matching given selector.
+ */
+export async function waitForFocus(selector: string): Promise<void> {
+  await driver.wait(async () => (await driver.find(selector).hasFocus()), 1000);
 }
 
 export async function waitForLabelInput(): Promise<void> {
