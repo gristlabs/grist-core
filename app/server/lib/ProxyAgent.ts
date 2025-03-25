2ef9ddcb61f6f6f1b55b312674cdb136502b61cf
fflorent <florent.git@zeteo.me>
2025-03-25 18:11:49 +0100

    Rename env var and rework module

--- app/server/lib/ProxyAgent.ts
+++ app/server/lib/ProxyAgent.ts
@@ -2,61 +2,84 @@ import {HttpsProxyAgent} from "https-proxy-agent";
 import {HttpProxyAgent} from "http-proxy-agent";
 import {appSettings} from "app/server/lib/AppSettings";
 import log from 'app/server/lib/log';
+import { StringUnion } from "app/common/StringUnion";
 
-function generateProxyConfigFromEnv() {
-  const proxyForTrustedRequestsUrl = appSettings.section('proxy').readString({
-    envVar: ['HTTPS_PROXY', 'https_proxy'],
-    preferredEnvVar: 'HTTPS_PROXY',
-  });
+const SupportedProtocols = StringUnion("http:", "https:");
+type SupportedProtocols = typeof SupportedProtocols.type;
 
-  const proxyForUntrustedRequestsUrl = appSettings.section('proxy').readString({
-    envVar: ['HTTPS_PROXY_FOR_UNTRUSTED_URLS', 'GRIST_HTTPS_PROXY'],
-    preferredEnvVar: 'HTTPS_PROXY_FOR_UNTRUSTED_URLS'
-  });
-
-  if (process.env.GRIST_HTTPS_PROXY) {
-    log.warn('GRIST_HTTPS_PROXY is deprecated in favor of HTTPS_PROXY_FOR_UNTRUSTED_URLS. ' +
-      `Please rather rather set HTTPS_PROXY_FOR_UNTRUSTED_URLS="${proxyForTrustedRequestsUrl}"`);
-  }
-
-  return {proxyForTrustedRequestsUrl, proxyForUntrustedRequestsUrl};
+interface Deps {
+  trusted: {
+    "http:": HttpProxyAgent;
+    "https:": HttpsProxyAgent;
+  } | undefined;
+  untrusted: {
+    "http:": HttpProxyAgent;
+    "https:": HttpsProxyAgent;
+  } | undefined;
 }
 
-export const test_generateProxyConfigFromEnv = generateProxyConfigFromEnv;
-
-// Read configuration from env on module load.
-export const Deps = generateProxyConfigFromEnv();
-
-function proxyAgent(requestUrl: URL, proxy: string): HttpProxyAgent | HttpsProxyAgent | undefined {
-  const ProxyAgent = requestUrl.protocol === "https:" ? HttpsProxyAgent : HttpProxyAgent;
-  const agent = new ProxyAgent(proxy);
+function buildProxyAgent<ProxyAgent extends HttpProxyAgent|HttpsProxyAgent>(
+  proxy: string, proxyAgentCtor: { new(proxy: string): ProxyAgent }
+): ProxyAgent {
+  const agent = new proxyAgentCtor(proxy);
 
   // Wrap the main method of ProxyAgent into a wrapper that logs errors.
   const callback = agent.callback;
-  agent.callback = async function () {
+  agent.callback = async function (...args: Parameters<HttpProxyAgent["callback"]>) {
+    const req = args[0];
     try {
-      return await callback.apply(this, arguments as any);
+      return await callback.call(this, ...args);
     } catch (e) {
       // Include info helpful for diagnosing issues (but not the potentially sensitive full requestUrl).
       log.rawWarn(`ProxyAgent error ${e}`,
-        {proxy, reqProtocol: requestUrl.protocol, requestOrigin: requestUrl.origin});
+        {proxy, reqProtocol: req.protocol, requestHost: req.host});
       throw e;
     }
   };
   return agent;
 }
 
-export function proxyAgentForTrustedRequests(requestUrl: URL): HttpProxyAgent | HttpsProxyAgent | undefined {
-  if (!Deps.proxyForTrustedRequestsUrl) {
-    return undefined;
+function generateProxyAgents(): Deps {
+  const proxyForTrustedRequestsUrl = appSettings.section('proxy').readString({
+    envVar: ['HTTPS_PROXY', 'https_proxy'],
+    preferredEnvVar: 'HTTPS_PROXY',
+  });
+
+  const proxyForUntrustedRequestsUrl = appSettings.section('proxy').readString({
+    envVar: ['GRIST_PROXY_FOR_UNTRUSTED_URLS', 'GRIST_HTTPS_PROXY'],
+    preferredEnvVar: 'GRIST_PROXY_FOR_UNTRUSTED_URLS'
+  });
+
+  if (process.env.GRIST_HTTPS_PROXY) {
+    log.warn('GRIST_HTTPS_PROXY is deprecated in favor of GRIST_PROXY_FOR_UNTRUSTED_URLS. ' +
+      `Please rather set GRIST_PROXY_FOR_UNTRUSTED_URLS="${proxyForTrustedRequestsUrl}"`);
   }
-  return proxyAgent(requestUrl, Deps.proxyForTrustedRequestsUrl);
+
+  return {
+    trusted: proxyForTrustedRequestsUrl ? {
+      "http:": buildProxyAgent(proxyForTrustedRequestsUrl, HttpProxyAgent),
+      "https:": buildProxyAgent(proxyForTrustedRequestsUrl, HttpsProxyAgent),
+    } : undefined,
+    untrusted: proxyForUntrustedRequestsUrl && proxyForUntrustedRequestsUrl !== "direct" ? {
+      "http:": buildProxyAgent(proxyForUntrustedRequestsUrl, HttpProxyAgent),
+      "https:": buildProxyAgent(proxyForUntrustedRequestsUrl, HttpsProxyAgent)
+    } : undefined
+  };
+}
+
+export const test_generateProxyAgents = generateProxyAgents;
+
+// Instantiate all the possible agents at startup.
+export const Deps = {
+  agents: generateProxyAgents()
+};
+
+export function proxyAgentForTrustedRequests(requestUrl: URL): HttpProxyAgent | HttpsProxyAgent | undefined {
+  const protocol = SupportedProtocols.check(requestUrl.protocol);
+  return Deps.agents.trusted?.[protocol];
 }
 
 export function proxyAgentForUntrustedRequests(requestUrl: URL): HttpProxyAgent | HttpsProxyAgent | undefined {
-
-  if (Deps.proxyForUntrustedRequestsUrl === "direct" || !Deps.proxyForUntrustedRequestsUrl) {
-    return undefined;
-  }
-  return proxyAgent(requestUrl, Deps.proxyForUntrustedRequestsUrl);
+  const protocol = SupportedProtocols.check(requestUrl.protocol);
+  return Deps.agents.untrusted?.[protocol];
 }
