d0aeb2f96dbea3b06e58ebba6298ed4a193fdbfe
Jordi Gutiérrez Hermoso <jordigh@octave.org>
2025-03-26 14:48:34 -0400

    updateChecker: new function, `compareWithLatest`
    Simple function to hit the remote endpoint and save the result to the
    home db, so it's available to the config.

--- app/server/lib/updateChecker.ts
+++ app/server/lib/updateChecker.ts
@@ -1,6 +1,7 @@
 import {Response as ExpressRequest} from 'express';
 import {commonUrls} from "app/common/gristUrls";
-import {version as installedVersion} from "app/common/version";
+import {version as installedVersion, LatestVersionAvailable} from "app/common/version";
+import {naturalCompare} from 'app/common/SortFunc';
 import {GristServer} from "app/server/lib/GristServer";
 
 export async function checkForUpdates(gristServer: GristServer, res: ExpressRequest|null) {
@@ -36,4 +37,19 @@ export async function checkForUpdates(gristServer: GristServer, res: ExpressRequ
     }
   }
   return await response.json();
+}
+
+export async function updateGristServerLatestVersion(gristServer: GristServer) {
+  const response = await checkForUpdates(gristServer, null);
+
+  // naturalCompare correctly sorts version numbers.
+  const versions = [installedVersion, response.latestVersion];
+  versions.sort(naturalCompare);
+
+  const latestVersionAvailable: LatestVersionAvailable = {
+    version: response.latestVersion,
+    isNewer: versions[1] !== installedVersion,
+  };
+
+  gristServer.setLatestVersionAvailable(latestVersionAvailable);
 }
--- stubs/app/common/version.ts
+++ stubs/app/common/version.ts
@@ -3,3 +3,8 @@ import packageJson from 'package.json';
 export const version = packageJson.version;
 export const channel = "core";
 export const gitcommit = "unknown";
+
+export interface LatestVersionAvailable {
+  version: string;
+  isNewer: boolean;
+}
