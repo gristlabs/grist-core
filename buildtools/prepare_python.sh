815734e8ebcc16cd9d711484a70d43f86a62662d
Grégoire Cutzach <gregoire@cutzach.com>
2025-04-17 17:55:57 +0200

    Update buildtools/prepare_python.sh
    Co-authored-by: Florent <florent.git@zeteo.me>
    
    feat: merge prepare_python.sh and prepare_python3.sh

--- buildtools/prepare_python.sh
+++ buildtools/prepare_python.sh
@@ -10,7 +10,15 @@ for possible_path in python/bin/python python/bin/python3 \
                      python/Scripts/python.exe python/python.exe; do
   if [[ -e $possible_path ]]; then
     echo "found $possible_path"
-    buildtools/prepare_python3.sh $possible_path python
+    if [[ -e sandbox_venv3 ]]; then
+     echo "Have Python3 sandbox"
+      exit 0
+    fi
+    echo "Updating Python3 packages"
+    $possible_path -m pip install --no-deps -r sandbox/requirements.txt
+    echo "Moving ./python to sandbox_venv3"
+    mv ./python sandbox_venv3
+    echo "Python3 packages ready in sandbox_venv3"
     exit 0
   fi
 done
@@ -20,5 +28,9 @@ echo "Use Python3 if available and recent enough"
 ! python3 -c 'import sys; assert sys.version_info >= (3,9)' 2> /dev/null && echo "Error: python must be >= 3.9" && exit 1
 
 # Default to python3 if recent enough.
-buildtools/prepare_python3.sh python3
+echo "Making Python3 sandbox"
+python3 -m venv sandbox_venv3
+echo "Updating Python3 packages"
+sandbox_venv3/bin/pip install --no-deps -r sandbox/requirements.txt
+echo "Python3 packages ready in sandbox_venv3"
 exit 0
