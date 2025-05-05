45b004e251adb216aaed6d5d8986188a2cb6f2d2
Spoffy <contact@spoffy.net>
2025-05-05 23:46:00 +0100

    Removes unused tooltip

--- app/client/ui/GristTooltips.ts
+++ app/client/ui/GristTooltips.ts
@@ -69,7 +69,6 @@ export type Tooltip =
   | 'reassignTwoWayReference'
   | 'attachmentStorage'
   | 'uploadAttachments'
-  | 'attachmentsNotIncluded'
   | 'adminControls'
   ;
 
@@ -242,16 +241,6 @@ see or edit which parts of your document.')
         "This allows you to add attachments that are missing from external storage, e.g. in an imported document. " +
         "Only .tar attachment archives downloaded from Grist can be uploaded here."
       ),
-    ),
-    ...args,
-  ),
-  attachmentsNotIncluded: (...args: DomElementArg[]) => cssTooltipContent(
-    cssMarkdownSpan(
-      t(
-        "If you want to re-import this document, " +
-        "a separate attachments archive will be needed to restore attachments. "
-      ),
-      dom('div', cssLink({href: "", target: "_blank"}, t('Learn more.'))),
     ),
     ...args,
   ),
