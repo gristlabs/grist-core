16c61f12c2fde3b9b707e4bce27804ccf9776449
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2025-05-06 15:18:39 -0400

    check column names

--- test/nbrowser/importerTestUtils.ts
+++ test/nbrowser/importerTestUtils.ts
@@ -41,8 +41,9 @@ export const waitForDiffPreviewToLoad = async (): Promise<void> => {
 
   // Check if we can see row number 1
   await driver.findContentWait('.test-importer-preview .gridview_data_row_num', "1", 5000);
+  console.log({colNames: await gu.getColumnNames()});
   await gu.waitForServer();
-  await driver.sleep(1000);
+  console.log({colNames: await gu.getColumnNames()});
 };
 
 // Helper that gets the list of visible column matching rows to the left of the preview.
