9435883a4d3c49e03ea697450fb9ba832ce9a730
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2025-05-06 15:56:40 -0400

    log a little more

--- do_the_thing.sh
+++ do_the_thing.sh
@@ -2,7 +2,13 @@
 
 set -e
 
+echo "DO THE THING $1 [$TESTS] [$GREP_TESTS]"
+
+echo "Clear test directory"
 rm -rf $TESTDIR || echo ok
+echo "Stop stray node"
 killall -9 node || echo ok
+echo "Stop stray chrome"
 killall -9 chrome || echo ok
+echo "Run tests"
 MOCHA_WEBDRIVER_HEADLESS=1 yarn run test:nbrowser
