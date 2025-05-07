551c5f99faaa5ef5a0ab454328b0cb097f5c4bca
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2025-05-07 16:54:38 -0400

    ApiConsole test occasionally fails
    The ApiConsole uses the swagger-ui which has some kind of
    transient state to elements from time to time. This adds
    a little retry around clicks on these elements.

--- test/nbrowser/ApiConsole.ts
+++ test/nbrowser/ApiConsole.ts
@@ -1,10 +1,10 @@
 import difference from 'lodash/difference';
-import {assert, driver} from 'mocha-webdriver';
+import {assert, driver, WebElementPromise} from 'mocha-webdriver';
 import * as gu from 'test/nbrowser/gristUtils';
 import {setupTestSuite} from 'test/nbrowser/testUtils';
 
 describe('ApiConsole', function () {
-  this.timeout(20000);
+  this.timeout(200000);
   const cleanup = setupTestSuite();
 
   before(async function () {
@@ -14,14 +14,16 @@ describe('ApiConsole', function () {
   });
 
   it('confirms delete operations before doing them', async function() {
+    for (let i = 0; i < 20; i++) {
+      console.log("Try", i);
     const myTab = await gu.myTab();
     await openApiConsolePage();
 
     try {
       // Start a DELETE operation but cancel it
-      await gu.scrollIntoView(driver.find('div#operations-orgs-deleteOrg')).click();
-      await driver.findWait('button.try-out__btn', 3000).click();
-      await driver.findWait('button.execute', 3000).click();
+      await clickWithRetry(() => gu.scrollIntoView(driver.find('div#operations-orgs-deleteOrg')));
+      await clickWithRetry(() => driver.findWait('button.try-out__btn', 3000));
+      await clickWithRetry(() => driver.findWait('button.execute', 3000));
       assert.equal(await driver.findWait('.test-modal-title', 3000).getText(),
                    'Confirm Deletion');
       await driver.findWait('button.test-modal-cancel', 3000).click();
@@ -31,9 +33,9 @@ describe('ApiConsole', function () {
       await gu.wipeToasts();
 
       // Start a DELETE operation and confirm it without writing "DELETE"
-      await driver.findWait('button.try-out__btn.cancel', 3000).click();
-      await driver.findWait('button.try-out__btn:not(.cancel)', 3000).click();
-      await driver.findWait('button.execute', 3000).click();
+      await clickWithRetry(() => driver.findWait('button.try-out__btn.cancel', 3000));
+      await clickWithRetry(() => driver.findWait('button.try-out__btn:not(.cancel)', 3000));
+      await clickWithRetry(() => driver.findWait('button.execute', 3000));
       await driver.findWait('button.test-modal-confirm', 3000).click();
       toasts = await gu.getToasts();
       assert.equal(toasts.length, 1);
@@ -41,8 +43,8 @@ describe('ApiConsole', function () {
       await gu.wipeToasts();
 
       // Start a DELETE operation and confirm it without writing "DELETE" correctly
-      await driver.findWait('button.try-out__btn.cancel', 3000).click();
-      await driver.findWait('button.try-out__btn:not(.cancel)', 3000).click();
+      await clickWithRetry(() => driver.findWait('button.try-out__btn.cancel', 3000));
+      await clickWithRetry(() => driver.findWait('button.try-out__btn:not(.cancel)', 3000));
       await driver.findWait('button.execute', 3000).click();
       await driver.findWait('input.test-modal-prompt', 3000).sendKeys('DELETENO');
       await driver.findWait('button.test-modal-confirm', 3000).click();
@@ -52,8 +54,8 @@ describe('ApiConsole', function () {
       await gu.wipeToasts();
 
       // Start a DELETE operation and confirm it with "DELETE"
-      await driver.findWait('button.try-out__btn.cancel', 3000).click();
-      await driver.findWait('button.try-out__btn:not(.cancel)', 3000).click();
+      await clickWithRetry(() => driver.findWait('button.try-out__btn.cancel', 3000));
+      await clickWithRetry(() => driver.findWait('button.try-out__btn:not(.cancel)', 3000));
       await driver.findWait('button.execute', 3000).click();
       await driver.findWait('input.test-modal-prompt', 3000).sendKeys('DELETE');
       await driver.findWait('button.test-modal-confirm', 3000).click();
@@ -63,6 +65,7 @@ describe('ApiConsole', function () {
       // There is an extra browser tab open for the api console.
       await driver.close();
       await myTab.open();
+    }
     }
   });
 });
@@ -76,4 +79,16 @@ async function openApiConsolePage() {
   assert.isDefined(newTab);
   await driver.switchTo().window(newTab);
   await driver.findContentWait('p', /An API for manipulating Grist/, 3000);
+}
+
+// The swagger-ui code is external, a little slow, a little async.
+// Elements can flicker in and out of existence. Maybe there is a
+// smart way to deal with this, but in the absence of that, here's
+// a little retry.
+async function clickWithRetry(finder: () => WebElementPromise) {
+  try {
+    await finder().click();
+  } catch (e) {
+    await finder().click();
+  }
 }
