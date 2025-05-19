e4a84dcd777cf2e86a7dc7f9ac38607857c7f596
Grégoire Cutzach <gregoire@cutzach.com>
2025-05-19 16:33:38 +0200

    wip: CRUD model ADDED

--- app/gen-server/lib/homedb/ServiceAccountsManager.ts
+++ app/gen-server/lib/homedb/ServiceAccountsManager.ts
@@ -3,7 +3,7 @@ import { HomeDBManager } from 'app/gen-server/lib/homedb/HomeDBManager';
 import { EntityManager } from 'typeorm';
 import { User } from 'app/gen-server/entity/User';
 import {v4 as uuidv4} from 'uuid';
-//import { ServiceAccount } from 'app/gen-server/entity/ServiceAccount';
+import { ServiceAccount } from 'app/gen-server/entity/ServiceAccount';
 
 export class ServiceAccountsManager {
 
@@ -39,17 +39,82 @@ export class ServiceAccountsManager {
       const uuid = uuidv4();
       const email = `${uuid}@serviceaccounts.local`;
       const serviceUser = await this._homeDb.getUserByLogin(email);
+      // End of life is set to now leading to a non functionning service service_account_id
+      // if not provided;
+      const endOfLifeString = endOfLife ?
+        endOfLife.toISOString().split('T')[0] :
+        new Date().toISOString().split('T')[0];
       // FIXME use manager.save(entité);
       return await manager.createQueryBuilder()
         .insert()
-        .into('service_accounts')
+        .into(ServiceAccount)
         .values({
-          ownerId,
-          serviceUserId: serviceUser.id,
+          owner_id: ownerId,
+          service_user_id: serviceUser.id,
           description,
-          endOfLife,
+          endOfLife: endOfLifeString,
         })
         .execute();
     });
   }
+
+  public async readServiceAccount(
+    serviceAccountId: number,
+    ownerId: number,
+  ){
+    await this._connection.transaction(async manager => {
+      return await manager.createQueryBuilder()
+        .select("*")
+        .from(ServiceAccount, "service_accounts")
+        .where(
+          "owner_id = :ownerId AND service_account_id = :serviceAccountId",
+          {
+            ownerId,
+            serviceAccountId
+          }
+        )
+        .execute();
+    });
+  }
+
+  public async updateServiceAccount(
+    serviceAccountId: number,
+    ownerId: number,
+    partial?: any,
+  ){
+    // FIXME Verify partial content
+    await this._connection.transaction(async manager => {
+      return await manager.createQueryBuilder()
+        .update(ServiceAccount)
+        .set(partial)
+        .where(
+          "owner_id = :ownerId AND service_account_id = :serviceAccountId",
+          {
+            ownerId,
+            serviceAccountId
+          }
+        )
+        .execute();
+    });
+  }
+
+  public async deleteServiceAccount(
+    serviceAccountId: number,
+    ownerId: number,
+  ){
+    await this._connection.transaction(async manager => {
+      return await manager.createQueryBuilder()
+        .delete()
+        .from(ServiceAccount)
+        .where(
+          "owner_id = :ownerId AND service_account_id = :serviceAccountId",
+          {
+            ownerId,
+            serviceAccountId
+          }
+        )
+        .execute();
+    });
+  }
+
 }
