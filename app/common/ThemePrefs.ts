7dd4267227735bf9e15d97458bd111ee6fee068e
Emmanuel Pelletier <manu@habite.la>
2025-05-28 12:18:03 +0200

    (kb) better visually mark when the active section is not focused
    this makes for a way easier to understand keyboard navigation between
    creator panel and widgets

--- app/common/ThemePrefs.ts
+++ app/common/ThemePrefs.ts
@@ -81,6 +81,7 @@ export const tokensCssMapping = {
   primaryMuted: 'primary-muted',
   primaryDim: 'primary-dim',
   primaryEmphasis: 'primary-emphasis',
+  primaryTransparent: 'primary-transparent',
 
   white: 'white',
   black: 'black',
@@ -260,6 +261,7 @@ export const componentsCssMapping = {
   widgetBg: 'widget-bg',
   widgetBorder: 'widget-border',
   widgetActiveBorder: 'widget-active-border',
+  widgetActiveNonFocusedBorder: 'widget-active-non-focused-border',
   widgetInactiveStripesLight: 'widget-inactive-stripes-light',
   widgetInactiveStripesDark: 'widget-inactive-stripes-dark',
   pinnedDocFooterBg: 'pinned-doc-footer-bg',
@@ -799,6 +801,11 @@ export interface SpecificThemeTokens {
    */
   primaryEmphasis: Token;
 
+  /**
+   * primary color with around 50% opacity
+   */
+  primaryTransparent: Token;
+
   controlBorderRadius: Token;
 
   /**
@@ -1101,6 +1108,7 @@ export interface BaseThemeTokens {
     widgetBg: Token;
     widgetBorder: Token;
     widgetActiveBorder: Token;
+    widgetActiveNonFocusedBorder: Token;
     widgetInactiveStripesLight: Token;
     pinnedDocFooterBg: Token;
     pinnedDocBorder: Token;
