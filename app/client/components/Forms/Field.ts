36f872ae0a6fcba018625c194c88b714f5f28c20
Florian RICHER <florian.richer@axyus.com>
2025-06-02 15:08:53 +0200

    Add support of Attachments

--- app/client/components/Forms/Field.ts
+++ app/client/components/Forms/Field.ts
@@ -677,8 +677,17 @@ class RefModel extends RefListModel {
 
 const AnyModel = TextModel;
 
-// Attachments are not currently supported.
-const AttachmentsModel = TextModel;
+class AttachmentsModel extends Question {
+  public renderInput() {
+    return dom('div',
+      css.cssAttachmentInput(
+        dom.prop('name', u => u(u(this.field).colId)),
+        dom.prop('type', 'file'),
+      ),
+      dom.style('width', '100%'),
+    );
+  }
+}
 
 function fieldConstructor(type: string): Constructor<Question> {
   switch (type) {
