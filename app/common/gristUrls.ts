0ba1d6a5864d92c5f39242e33b194dde985bd8a4
Jordi Gutiérrez Hermoso <jordigh@octave.org>
2025-06-13 16:00:59 -0400

    VersionUpdateBanner: display critical updates as errors
    The red colour should give it a greater sense of urgency.
    
    We currently don't have a mechanism to mark certain versions as being
    critical, but we can change the version-serving code later. This is
    just version-consuming code to handle a potential critical version
    being reported.

--- app/common/gristUrls.ts
+++ app/common/gristUrls.ts
@@ -754,8 +754,9 @@ export interface ActivationState {
 export interface LatestVersionAvailable {
   version: string;
   isNewer: boolean;
+  isCritical: boolean;
   dateChecked: number;
-  releaseUrl: string;
+  releaseUrl?: string;
 }
 
 /**
