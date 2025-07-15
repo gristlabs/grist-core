39d1faf938d1a4efc180c951070085f73389fac4
George Gevoian <george@gevoian.com>
2025-07-14 21:31:07 -0400

    Add new attachment rows quicker

--- app/client/widgets/AttachmentsEditor.ts
+++ app/client/widgets/AttachmentsEditor.ts
@@ -48,12 +48,6 @@ interface Attachment {
  * download, add or remove attachments in the edited cell.
  */
 export class AttachmentsEditor extends NewBaseEditor {
-  public static skipEditor(typedVal: CellValue|undefined, origVal: CellValue): CellValue|undefined {
-    if (Array.isArray(typedVal)) {
-      return typedVal;
-    }
-  }
-
   private _attachmentsTable: MetaTableData<'_grist_Attachments'>;
   private _docComm: DocComm;
 
