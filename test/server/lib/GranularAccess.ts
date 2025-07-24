16060e100b93cf8f018b2cba9874bb8159e90aa8
Thomas Guillet <thomas@palya.fr>
2025-07-24 17:33:24 +0200

    Adjust constant to convention

--- test/server/lib/GranularAccess.ts
+++ test/server/lib/GranularAccess.ts
@@ -3854,10 +3854,10 @@ describe("GranularAccess", function() {
     await assert.isRejected(postAttachment(editor, docId, readTokenResult.token, "dataError", "error.txt"));
 
     const tokenResult = (await cliEditor.send("getAccessToken", 0, {readOnly: false})).data;
-    const i7 = await postAttachment(editor, docId, tokenResult.token, "data7", "7.txt");
+    const i7 = await postAttachment(editor, docId, tokenResult.token, "content7", "7.txt");
     await assert.isFulfilled(getAttachment(owner, docId, i7));
     //await assert.isFulfilled(getAttachment(editor, docId, i7));
-    await editor.getDocAPI(docId).updateRows('Data1', {id: [1], Pics: [[GristObjCode.List, i7]]});
+    await editor.getDocAPI(docId).updateRows('Data1', {id: [1], Texts: [[GristObjCode.List, i7]]});
     await assert.isFulfilled(getAttachment(editor, docId, i7));
 
     // Attachment check is not applied for undos of actions by the same user.
