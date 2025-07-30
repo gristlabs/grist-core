33688cf96b5608b752c0a238e2946b2ed15c2ed2
fflorent <florent.fayolle+git@beta.gouv.fr>
2025-07-30 18:41:26 +0200

    Add missing files

---
+++ app/common/ServiceAccount.ts
@@ -0,0 +1,7 @@
+export interface PostServiceAccount {
+  label: string|undefined;
+  description: string|undefined;
+  endOfLife: string|undefined;
+}
+
+export type PatchServiceAccount = PostServiceAccount
