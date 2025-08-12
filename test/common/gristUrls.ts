bf8be4806515d580fe511ec9c5f72a1f68f3cfd6
fflorent <florent.fayolle+git@beta.gouv.fr>
2025-08-12 18:46:36 +0200

    Move tests

--- test/common/gristUrls.ts
+++ test/common/gristUrls.ts
@@ -1,6 +1,5 @@
 import {
-  decodeUrl, commonUrls as defaultCommonUrls, getCommonUrls,
-  getHostType, getSlugIfNeeded, IGristUrlState, parseFirstUrlPart
+  decodeUrl, getHostType, getSlugIfNeeded, IGristUrlState, parseFirstUrlPart
 } from 'app/common/gristUrls';
 import {assert} from 'chai';
 import Sinon from 'sinon';
@@ -164,96 +163,6 @@ describe('gristUrls', function() {
       assert.strictEqual(getSlugIfNeeded({id, urlId, name: "Hélène's résumé"}), 'Helenes-resume');
       assert.strictEqual(getSlugIfNeeded({id, urlId, name: "Привіт, Їжак!"}), 'Privit-Yizhak');
       assert.strictEqual(getSlugIfNeeded({id, urlId, name: "S&P500 is ~$4,894.16"}), 'SandP500-is-dollar489416');
-    });
-  });
-
-  describe('getCommonUrls', function () {
-    it('should return the default URLs', function () {
-      const commonUrls = getCommonUrls();
-      assert.isObject(commonUrls);
-      assert.equal(commonUrls.help, "https://support.getgrist.com");
-    });
-
-    describe("with GRIST_CUSTOM_COMMON_URLS env var set", function () {
-      it('should return the values set by the GRIST_CUSTOM_COMMON_URLS env var', function () {
-        const customHelpCenterUrl = "http://custom.helpcenter";
-        sandbox.define(process.env, 'GRIST_CUSTOM_COMMON_URLS',
-          `{"help": "${customHelpCenterUrl}"}`);
-        const commonUrls = getCommonUrls();
-        assert.isObject(commonUrls);
-        assert.equal(commonUrls.help, customHelpCenterUrl);
-        assert.equal(commonUrls.helpAccessRules, "https://support.getgrist.com/access-rules");
-      });
-
-      it('should throw when keys extraneous to the ICommonUrls interface are added', function () {
-        const nonExistingKey = 'iDontExist';
-        sandbox.define(process.env, 'GRIST_CUSTOM_COMMON_URLS',
-          `{"${nonExistingKey}": "foo", "help": "https://getgrist.com"}`);
-        assert.throws(() => getCommonUrls(), `value.${nonExistingKey} is extraneous`);
-      });
-
-      it('should throw when the passed JSON is malformed', function () {
-        sandbox.define(process.env, 'GRIST_CUSTOM_COMMON_URLS', '{"malformed": 42');
-        assert.throws(() => getCommonUrls(), 'The JSON passed to GRIST_CUSTOM_COMMON_URLS is malformed');
-      });
-
-      it('should throw when keys has unexpected type', function () {
-        const regularValueKey = 'help';
-        const numberValueKey = 'helpAccessRules';
-        const objectValueKey = 'helpAssistant';
-        const arrayValueKey = 'helpAssistantDataUse';
-        const nullValueKey = 'helpFormulaAssistantDataUse';
-
-        sandbox.define(process.env, 'GRIST_CUSTOM_COMMON_URLS',
-          JSON.stringify({
-            [regularValueKey]: "https://getgrist.com",
-            [numberValueKey]: 42,
-            [objectValueKey]: {"key": "value"},
-            [arrayValueKey]: ["foo"],
-          })
-        );
-        const buildExpectedErrRegEx = (...keys: string[]) => new RegExp(
-          keys.map(key => `value\\.${key}`).join('.*'),
-          'ms'
-        );
-        assert.throws(() => getCommonUrls(), buildExpectedErrRegEx(numberValueKey, objectValueKey, arrayValueKey));
-        sandbox.restore();
-        sandbox.define(process.env, 'GRIST_CUSTOM_COMMON_URLS',
-          JSON.stringify({
-            [regularValueKey]: "https://getgrist.com",
-            [nullValueKey]: null,
-          })
-        );
-        assert.throws(() => getCommonUrls(), buildExpectedErrRegEx(nullValueKey));
-      });
-
-      it("should return the default URLs when the parsed value is not an object", function () {
-        sandbox.define(process.env, "GRIST_CUSTOM_COMMON_URLS", "42");
-        assert.deepEqual(getCommonUrls(), defaultCommonUrls);
-        sandbox.restore();
-        sandbox.define(process.env, "GRIST_CUSTOM_COMMON_URLS", "null");
-        assert.deepEqual(getCommonUrls(), defaultCommonUrls);
-      });
-    });
-
-    describe("client-side when customized by the admin", function () {
-      it("should read the admin-defined values gristConfig", function () {
-        sandbox.define(globalThis, 'window', {
-          gristConfig: {
-            adminDefinedUrls: JSON.stringify({
-              help: "https://getgrist.com"
-            })
-          },
-          // Fake location to make isClient() believe the code is executed client-side.
-          location: {
-            hostname: 'getgrist.com'
-          },
-        });
-        const commonUrls = getCommonUrls();
-        assert.isObject(commonUrls);
-        assert.equal(commonUrls.help, "https://getgrist.com");
-        assert.equal(commonUrls.helpAccessRules, "https://support.getgrist.com/access-rules");
-      });
     });
   });
 });
