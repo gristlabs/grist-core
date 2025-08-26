086711c088858664d0c08f2f8399072d3677c331
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2025-08-26 11:14:09 -0400

    remove duplicate

--- app/common/CommTypes.ts
+++ app/common/CommTypes.ts
@@ -8,8 +8,7 @@ import {AttachmentTransferStatus} from 'app/common/UserAPI';
 
 export const ValidEvent = StringUnion(
   'docListAction', 'docUserAction', 'docShutdown', 'docError',
-  'docUsage', 'docChatter', 'docUserPresenceUpdate', 'clientConnect',
-  'docUserPresenceUpdate');
+  'docUsage', 'docChatter', 'docUserPresenceUpdate', 'clientConnect');
 export type ValidEvent = typeof ValidEvent.type;
 
 
