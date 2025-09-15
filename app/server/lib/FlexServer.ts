gristlabs/grist-core

commit 43faaba2cb19ef3c83ef3a2b9553dd0534eee69e

(core) Refactor and clean up the organization of Notifier code.
Summary:
Notifier-related code has gotten confusing. Some examples: class called
"Notifier" was specific to sendgrid; type called "NotifierEvents" lived in
HomeDBManager where it wasn't used; the EmitNotifier with a critical role was
defined in FlexServer.

Changes made in this diff:
- Removed NotifierEvents declaration from HomeDBManager. It turns out to be
  unnecessary.
- Renamed 'Notifier' to 'SendGridNotifier' (both the file and the class), to be
  clearer on what it actually is.
- Merged configureTestNotifier.ts into TestNotifier.ts to simplify code
  organization.
- Moved EmitNotifier from FlexServer to INotifier.
- Refactored EmitNotifier to explicitly delegate to a primary INotifier; when
  not set, it behaves as EmptyNotifier did. (That also removes the need for
  NotifierEvents type.)
- Removed NotifierBase and EmptyNotifier.

This automatically fixes the issue with 'deleteUser' notifier method not
getting called, since it's no longer relying on NotifierEvents being kept in
sync (that's what was missing this method).

Test Plan:
- There should be no behavior differences, except for no longer ignoring
  "deleteUser" and "streamingDestinationsChange" events, which were lost due to
  being omitted in NotifierEvents.
- Add a test case that SendGridNotifier's deleteUser method now gets called;
  and I checked that the same test case would fail on master.

Reviewers: jarek

Reviewed By: jarek

Differential Revision: https://phab.getgrist.com/D4715

app/server/lib/FlexServer.ts
@@ -19,7 +19,7 @@ import {ActivationsManager} from 'app/gen-server/lib/ActivationsManager';
 import {DocApiForwarder} from 'app/gen-server/lib/DocApiForwarder';
 import {getDocWorkerMap} from 'app/gen-server/lib/DocWorkerMap';
 import {Doom} from 'app/gen-server/lib/Doom';
-import {HomeDBManager, NotifierEvents, UserChange} from 'app/gen-server/lib/homedb/HomeDBManager';
+import {HomeDBManager, UserChange} from 'app/gen-server/lib/homedb/HomeDBManager';
 import {Housekeeper} from 'app/gen-server/lib/Housekeeper';
 import {Usage} from 'app/gen-server/lib/Usage';
 import {AccessTokens, IAccessTokens} from 'app/server/lib/AccessTokens';
@@ -62,7 +62,7 @@ import {IAuditLogger} from 'app/server/lib/IAuditLogger';
 import {IBilling} from 'app/server/lib/IBilling';
 import {IDocNotificationManager} from 'app/server/lib/IDocNotificationManager';
 import {IDocStorageManager} from 'app/server/lib/IDocStorageManager';
-import {EmptyNotifier, INotifier, TestSendGridExtensions} from 'app/server/lib/INotifier';
+import {EmitNotifier, INotifier} from 'app/server/lib/INotifier';
 import {InstallAdmin} from 'app/server/lib/InstallAdmin';
 import log, {logAsJson} from 'app/server/lib/log';
 import {IPermitStore} from 'app/server/lib/Permit';
@@ -88,7 +88,6 @@ import {addUploadRoute} from 'app/server/lib/uploads';
 import {buildWidgetRepository, getWidgetsInPlugins, IWidgetRepository} from 'app/server/lib/WidgetRepository';
 import {setupLocale} from 'app/server/localization';
 import axios from 'axios';
-import EventEmitter from 'events';
 import express from 'express';
 import * as fse from 'fs-extra';
 import * as http from 'http';
@@ -174,7 +173,6 @@ export class FlexServer implements GristServer {
   private _docWorkerMap: IDocWorkerMap;
   private _docWorkerLoadTracker?: DocWorkerLoadTracker;
   private _widgetRepository: IWidgetRepository;
-  private _notifier: INotifier;
   private _docNotificationManager: IDocNotificationManager|undefined|false = false;
   private _pubSubManager: IPubSubManager = createPubSubManager(process.env.REDIS_URL);
   private _assistant?: IAssistant;
@@ -211,8 +209,7 @@ export class FlexServer implements GristServer {
   private _updateManager: UpdateManager;
   private _sandboxInfo: SandboxInfo;
   private _jobs?: GristJobs;
-  private _emitNotifier = new EmitNotifier();
-  private _testPendingNotifications: number = 0;
+  private _emitNotifier: EmitNotifier = new EmitNotifier();
   private _latestVersionAvailable?: LatestVersionAvailable;
 
   constructor(public port: number, public name: string = 'flexServer',
@@ -454,12 +451,12 @@ export class FlexServer implements GristServer {
   }
 
   public hasNotifier(): boolean {
-    return Boolean(this._notifier) && this._notifier !== EmptyNotifier;
+    return !this._emitNotifier.isEmpty();
   }
 
   public getNotifier(): INotifier {
     // Check that our internal notifier implementation is in place.
-    if (!this._notifier) { throw new Error('no notifier available'); }
+    if (!this.hasNotifier()) { throw new Error('no notifier available'); }
     // Expose a wrapper around it that emits actions.
     return this._emitNotifier;
   }
@@ -1947,21 +1944,10 @@ export class FlexServer implements GristServer {
     // and all that is needed is a refactor to pass that info along.  But there is also the
     // case of notification(s) from stripe.  May need to associate a preferred base domain
     // with org/user and persist that?
-    this._notifier = this.create.Notifier(this._dbManager, this);
-    for (const method of NotifierEvents.values) {
-      this._emitNotifier.on(method, async (...args) => {
-        this._testPendingNotifications++;
-        try {
-          await (this._notifier[method] as any)(...args);
-        } catch (e) {
-          // Catch error since as an event handler we can't return one.
-          log.error("Notifier failed:", e);
-        } finally {
-          this._testPendingNotifications--;
-        }
-      });
+    const primaryNotifier = this.create.Notifier(this._dbManager, this);
+    if (primaryNotifier) {
+      this._emitNotifier.setPrimaryNotifier(primaryNotifier);
     }
-    this._emitNotifier.sendGridExtensions = this._notifier.testSendGridExtensions?.();
 
     // For doc notifications, if we are a home server, initialize endpoints and job handling.
     this.getDocNotificationManager()?.initHomeServer(this.app);
@@ -1973,11 +1959,6 @@ export class FlexServer implements GristServer {
     if (this._assistant?.version === 2) {
       this._assistant?.addEndpoints?.(this.app);
     }
-  }
-
-  // for test purposes, check if any notifications are in progress
-  public get testPending(): boolean {
-    return this._testPendingNotifications > 0;
   }
 
   public getGristConfig(): GristLoadConfig {
@@ -2781,53 +2762,3 @@ const serveAnyOrigin: serveStatic.ServeStaticOptions = {
     res.setHeader("Access-Control-Allow-Origin", "*");
   }
 };
-
-/**
- *
- * Handle events that should result in notifications to users via
- * transactional emails (or future generalizations). Currently
- * handled by simply emitting them and hooking them up to a
- * sendgrid-based implementation (see FlexServer.addNotifier).
- * Some of the events are also distributed internally via
- * FlexServer.onUserChange and FlexServer.onStreamingDestinationsChange.
- * This might be a good point to replace some of this activity with
- * a job queue and queue workers. In particular, processing of
- * FlexServer.onUserChange could do with moving to a queue since
- * it may require communication with external billing software and
- * should be robust to delays and failures there. More generally,
- * if notications are subject to delays and failures and we wish to
- * be robust, a job queue would be a good idea for all of this.
- *
- * Although the interface this class implements is async, it is
- * best if the implementation remains fast and reliable. Any delays
- * here will impact API calls. Calls to place something in Redis
- * may be acceptable.
- */
-export class EmitNotifier extends EventEmitter implements INotifier {
-  public sendGridExtensions?: TestSendGridExtensions;
-
-  public addUser = this._wrapEvent('addUser');
-  public addBillingManager = this._wrapEvent('addBillingManager');
-  public firstLogin = this._wrapEvent('firstLogin');
-  public teamCreator = this._wrapEvent('teamCreator');
-  public userChange = this._wrapEvent('userChange');
-  public trialPeriodEndingSoon = this._wrapEvent('trialPeriodEndingSoon');
-  public trialingSubscription = this._wrapEvent('trialingSubscription');
-  public scheduledCall = this._wrapEvent('scheduledCall');
-  public streamingDestinationsChange = this._wrapEvent('streamingDestinationsChange');
-  public twoFactorStatusChanged = this._wrapEvent('twoFactorStatusChanged');
-  public docNotification = this._wrapEvent('docNotification');
-
-  // Pass on deleteUser in the same way
-  public deleteUser = this._wrapEvent('deleteUser');
-
-  public testSendGridExtensions() {
-    return this.sendGridExtensions;
-  }
-
-  private _wrapEvent<Name extends keyof INotifier>(eventName: Name): INotifier[Name] {
-    return (async (...args: any[]) => {
-      this.emit(eventName, ...args);
-    }) as INotifier[Name];
-  }
-}

app/gen-server/lib/Doom.ts
@@ -18,7 +18,7 @@ import fetch from 'node-fetch';
  */
 export class Doom {
   constructor(private _dbManager: HomeDBManager, private _permitStore: IPermitStore,
-              private _notifier: INotifier, private _loginSystem: GristLoginSystem,
+              private _notifier: Pick<INotifier, "deleteUser">, private _loginSystem: GristLoginSystem,
               private _homeApiUrl: string) {
   }
 

app/gen-server/lib/homedb/HomeDBManager.ts
@@ -18,7 +18,6 @@ import {UserProfile} from 'app/common/LoginSessionAPI';
 import {checkSubdomainValidity} from 'app/common/orgNameUtils';
 import {DocPrefs, FullDocPrefs} from 'app/common/Prefs';
 import * as roles from 'app/common/roles';
-import {StringUnion} from 'app/common/StringUnion';
 import {
   ANONYMOUS_USER_EMAIL,
   DocumentProperties,
@@ -91,7 +90,7 @@ import {appSettings} from 'app/server/lib/AppSettings';
 import {getOrCreateConnection} from 'app/server/lib/dbUtils';
 import {StorageCoordinator} from 'app/server/lib/GristServer';
 import {makeId} from 'app/server/lib/idUtils';
-import {EmptyNotifier, INotifier} from 'app/server/lib/INotifier';
+import {EmitNotifier, INotifier} from 'app/server/lib/INotifier';
 import log from 'app/server/lib/log';
 import {Permit} from 'app/server/lib/Permit';
 import {IPubSubManager} from 'app/server/lib/PubSubManager';
@@ -120,20 +119,6 @@ import {v4 as uuidv4} from 'uuid';
 applyPatch();
 
 export { SUPPORT_EMAIL };
-export const NotifierEvents = StringUnion(
-  'addUser',
-  'userChange',
-  'firstLogin',
-  'addBillingManager',
-  'teamCreator',
-  'trialPeriodEndingSoon',
-  'trialingSubscription',
-  'scheduledCall',
-  'twoFactorStatusChanged',
-  'docNotification',
-);
-
-export type NotifierEvent = typeof NotifierEvents.type;
 
 export const Deps = {
   defaultMaxNewUserInvitesPerOrg: {
@@ -309,7 +294,7 @@ export class HomeDBManager {
 
   public constructor(
     public storageCoordinator?: StorageCoordinator,
-    private _notifier: INotifier = EmptyNotifier,
+    private _notifier: INotifier = new EmitNotifier(),
     pubSubManager?: IPubSubManager,
   ) {
     this.caches = pubSubManager ? new HomeDBCaches(this, pubSubManager) : null;

app/server/lib/GristServer.ts
@@ -52,7 +52,6 @@ export interface StorageCoordinator {
  */
 export interface GristServer extends StorageCoordinator {
   readonly create: ICreate;
-  readonly testPending: boolean;
   settings?: IGristCoreConfig;
   getHost(): string;
   getHomeUrl(req: express.Request, relPath?: string): string;
@@ -169,7 +168,6 @@ export interface DocTemplate {
 export function createDummyGristServer(): GristServer {
   return {
     create,
-    testPending: false,
     settings: loadGristCoreConfig(),
     getHost() { return 'localhost:4242'; },
     getHomeUrl() { return 'http://localhost:4242'; },

app/server/lib/ICreate.ts
@@ -16,7 +16,7 @@ import {createNullAuditLogger, IAuditLogger} from 'app/server/lib/IAuditLogger';
 import {EmptyBilling, IBilling} from 'app/server/lib/IBilling';
 import {IDocNotificationManager} from 'app/server/lib/IDocNotificationManager';
 import {IDocStorageManager} from 'app/server/lib/IDocStorageManager';
-import {EmptyNotifier, INotifier} from 'app/server/lib/INotifier';
+import {INotifier} from 'app/server/lib/INotifier';
 import {InstallAdmin, SimpleInstallAdmin} from 'app/server/lib/InstallAdmin';
 import {ISandbox, ISandboxCreationOptions} from 'app/server/lib/ISandbox';
 import {createSandbox, SpawnFn} from 'app/server/lib/NSandbox';
@@ -66,7 +66,7 @@ export interface ICreate {
   ): Promise<IDocStorageManager>;
 
   Billing(dbManager: HomeDBManager, gristConfig: GristServer): IBilling;
-  Notifier(dbManager: HomeDBManager, gristConfig: GristServer): INotifier;
+  Notifier(dbManager: HomeDBManager, gristConfig: GristServer): INotifier|undefined;
   AuditLogger(dbManager: HomeDBManager, gristConfig: GristServer): IAuditLogger;
   Telemetry(dbManager: HomeDBManager, gristConfig: GristServer): ITelemetry;
   Assistant(gristConfig: GristServer): IAssistant|undefined;
@@ -129,8 +129,8 @@ export class BaseCreate implements ICreate {
   public Billing(dbManager: HomeDBManager, gristConfig: GristServer): IBilling {
     return new EmptyBilling();
   }
-  public Notifier(dbManager: HomeDBManager, gristConfig: GristServer): INotifier {
-    return EmptyNotifier;
+  public Notifier(dbManager: HomeDBManager, gristConfig: GristServer): INotifier|undefined {
+    return undefined;
   }
   public ExternalStorage(...[purpose, extraPrefix]: Parameters<ExternalStorageCreator>): ExternalStorage|undefined {
     for (const s of this._storage) {

app/server/lib/INotifier.ts
@@ -1,3 +1,27 @@
+/**
+ * INotifier defines the interface for events that should result in notifications to users
+ * via transactional emails (or future generalizations).
+ *
+ * Although this interface is async, it is best if the implementation
+ * remains very fast and reliable. Any delays here will impact API
+ * calls. Calls to place something in Redis may be acceptable.
+ * Retrying email notifications, no.
+ *
+ * In practice, the notifier that does the delivery of notifications is wrapped into an
+ * EmitNotifier, with fire-and-forget semantics. Its methods return without waiting for the async
+ * calls.
+ *
+ * EmitNotifier also distributes events to other internal subscribers, e.g. via
+ * FlexServer.onUserChange and FlexServer.onStreamingDestinationsChange.
+ *
+ * Some notifier activity ought to be replaced with
+ * a job queue and queue workers. In particular, processing of
+ * FlexServer.onUserChange could do with moving to a queue since
+ * it may require communication with external billing software and
+ * should be robust to delays and failures there. More generally,
+ * if notications are subject to delays and failures and we wish to
+ * be robust, a job queue would be a good idea for all of this.
+ */
 import { FullUser } from 'app/common/LoginSessionAPI';
 import * as roles from 'app/common/roles';
 import { BillingAccount } from 'app/gen-server/entity/BillingAccount';
@@ -8,18 +32,10 @@ import { Workspace } from 'app/gen-server/entity/Workspace';
 import { UserChange, UserIdDelta } from 'app/gen-server/lib/homedb/HomeDBManager';
 import { SendGridConfig, SendGridMailWithTemplateId, TwoFactorEvent } from 'app/gen-server/lib/NotifierTypes';
 import { DocNotificationEvent, DocNotificationTemplateBase } from 'app/gen-server/lib/NotifierTypes';
+import log from 'app/server/lib/log';
+import { EventEmitter } from 'events';
 
-/**
- *
- * Interface for events that should result in notifications to users
- * via transactional emails (or future generalizations).
- *
- * Although this interface is async, it is best if the implementation
- * remains very fast and reliable. Any delays here will impact API
- * calls. Calls to place something in Redis may be acceptable.
- * Retrying email notifications, no.
- */
-export interface INotifier {
+interface INotifierMethods {
   /**
    * These methods are all called when a thing happens, with
    * the intent to notify anyone who should know.
@@ -58,7 +74,9 @@ export interface INotifier {
    * e.g. to remove from any email lists.
    */
   deleteUser(userId: number): Promise<void>;
+}
 
+export interface INotifier extends INotifierMethods {
   /**
    * For old tests, we preserve some weird old methods.
    * This may need further refactoring or elimination.
@@ -76,34 +94,53 @@ export interface TestSendGridExtensions {
 }
 
 /**
- * A base notifier class that by default does nothing.
+ * EmitNotifier wraps another INotifier, but introduces two differences:
+ * - The wrapped INotifier is optional; if not set via setPrimaryNotifier(), it's just not called.
+ * - Each call returns immediately; any errors in the underlying async call are caught and logged.
+ * - It is an EventEmitter; for every INotifier method it emits an event with the same name and
+ *   the same arguments, to allow other code to subscribe.
  */
-export class BaseNotifier implements INotifier {
-  public async addUser(
-    _userId: number,
-    _resource: Organization | Workspace | Document,
-    _delta: UserIdDelta,
-    _membersBefore: Map<roles.NonGuestRole, User[]>
-  ): Promise<void> {}
-  public async addBillingManager(_hostUserId: number, _addUserId: number, _orgs: Organization[]): Promise<void> {}
-  public async firstLogin(_user: FullUser): Promise<void> {}
-  public async teamCreator(_userId: number): Promise<void> {}
-  public async userChange(_change: UserChange): Promise<void> {}
-  public async trialPeriodEndingSoon(
-    _account: BillingAccount,
-    _subscription: { trial_end: number | null }): Promise<void> {}
-  public async trialingSubscription(_account: BillingAccount): Promise<void> {}
-  public async scheduledCall(_userRef: string): Promise<void> {}
-  public async streamingDestinationsChange(_orgId: number | null): Promise<void> {}
-  public async twoFactorStatusChanged(_event: TwoFactorEvent, _userId: number,
-                                      _method?: 'TOTP' | 'SMS'): Promise<void> {}
+export class EmitNotifier extends EventEmitter implements INotifier {
+  public addUser = this._wrapEvent('addUser');
+  public addBillingManager = this._wrapEvent('addBillingManager');
+  public firstLogin = this._wrapEvent('firstLogin');
+  public teamCreator = this._wrapEvent('teamCreator');
+  public userChange = this._wrapEvent('userChange');
+  public trialPeriodEndingSoon = this._wrapEvent('trialPeriodEndingSoon');
+  public trialingSubscription = this._wrapEvent('trialingSubscription');
+  public scheduledCall = this._wrapEvent('scheduledCall');
+  public streamingDestinationsChange = this._wrapEvent('streamingDestinationsChange');
+  public twoFactorStatusChanged = this._wrapEvent('twoFactorStatusChanged');
+  public docNotification = this._wrapEvent('docNotification');
+  public deleteUser = this._wrapEvent('deleteUser');
 
-  public async docNotification(event: DocNotificationEvent, userId: number, templateData: object): Promise<void> {}
-  public async deleteUser(_userId: number) {}
+  private _primaryNotifier: INotifier|null = null;
+  private _testPendingNotifications = 0;
+
+  public setPrimaryNotifier(notifier: INotifier) { this._primaryNotifier = notifier; }
+
+  public isEmpty() { return !this._primaryNotifier; }
+
+  public testSendGridExtensions() { return this._primaryNotifier?.testSendGridExtensions?.(); }
+  public testPendingNotifications(): number { return this._testPendingNotifications; }
+
+  private _wrapEvent<Name extends keyof INotifierMethods>(methodName: Name): INotifier[Name] {
+    return async (...args: any[]) => {
+      this._callPrimary(methodName, ...args)
+        .catch(e => log.error("Notifier failed", e));
+
+      // Also emit as an event that others could listen to.
+      this.emit(methodName, ...args);
+    };
+  }
+
+  private async _callPrimary(methodName: keyof INotifierMethods, ...args: any[]) {
+    if (!this._primaryNotifier) { return; }
+    this._testPendingNotifications++;
+    try {
+      await (this._primaryNotifier[methodName] as any)(...args);
+    } finally {
+      this._testPendingNotifications--;
+    }
+  }
 }
-
-/**
- * A notifier instance that does nothing. Used when no email
- * notifications are configured.
- */
-export const EmptyNotifier: INotifier = new BaseNotifier();

test/gen-server/ApiServerAccess.ts
@@ -8,14 +8,13 @@ import {HomeDBManager, Deps as HomeDBManagerDeps, UserChange} from 'app/gen-serv
 import {SendGridConfig, SendGridMailWithTemplateId} from 'app/gen-server/lib/NotifierTypes';
 import {create} from 'app/server/lib/create';
 import axios, {AxiosResponse} from 'axios';
-import {delay} from 'bluebird';
 import * as chai from 'chai';
 import fromPairs = require('lodash/fromPairs');
 import pick = require('lodash/pick');
 import moment from 'moment';
 import * as sinon from 'sinon';
 import {TestServer} from 'test/gen-server/apiUtils';
-import {configForUser} from 'test/gen-server/testUtils';
+import {configForUser, waitForAllNotifications} from 'test/gen-server/testUtils';
 import * as testUtils from 'test/server/testUtils';
 import * as roles from 'app/common/roles';
 
@@ -94,17 +93,11 @@ describe('ApiServerAccess', function() {
   });
 
   async function getLastMail(maxWait: number = 1000) {
-    const start = Date.now();
-    while (Date.now() - start < maxWait) {
-      if (!server.server.testPending) {
-        const result = {payload: lastMail, description: lastMailDesc};
-        lastMailDesc = null;
-        lastMail = null;
-        return result;
-      }
-      await delay(1);
-    }
-    throw new Error('getMessages timed out');
+    await waitForAllNotifications(server.server, maxWait);
+    const result = {payload: lastMail, description: lastMailDesc};
+    lastMailDesc = null;
+    lastMail = null;
+    return result;
   }
 
   async function assertLastMail(maxWait: number = 1000) {

test/gen-server/apiUtils.ts
@@ -1,4 +1,3 @@
-import {delay} from 'app/common/delay';
 import {Role} from 'app/common/roles';
 import {UserAPIImpl, UserProfile} from 'app/common/UserAPI';
 import {AclRule, AclRuleDoc, AclRuleOrg, AclRuleWs} from 'app/gen-server/entity/AclRule';
@@ -20,7 +19,7 @@ import FormData from 'form-data';
 import fetch from 'node-fetch';
 import * as path from 'path';
 import {createInitialDb, removeConnection, setUpDB} from 'test/gen-server/seed';
-import {setPlan} from 'test/gen-server/testUtils';
+import {setPlan, waitForAllNotifications} from 'test/gen-server/testUtils';
 import {fixturesRoot} from 'test/server/testUtils';
 import {isAffirmative} from 'app/common/gutil';
 
@@ -58,10 +57,7 @@ export class TestServer {
     // and node-sqlite3 has become fussier about this, and in regular tests
     // we substitute sqlite for postgres.
     if (this.server.hasNotifier()) {
-      for (let i = 0; i < 30; i++) {
-        if (!this.server.testPending) { break; }
-        await delay(100);
-      }
+      await waitForAllNotifications(this.server, 3000);
     }
     await removeConnection();
   }

commit 6553c8e68c335eb4085b05dcd7bf1f21ce1563ff

Restore recently-broken notifier: getNotifier() should succeed even if notifier is empty. (#1841)
Prior to 43faaba, getNotifier() wasn't using the hasNotifier() check,
but succeeded whenever a notifier was "configured" (by adding
addNotifier()), even if it was configured with the "empty notifier".

This restores previous behavior. Any home server calls addNotifier(),
and the updated check restores the behavior of only checking if
addNotifier() has been called.

## Has this been tested?

Tests have been failing since 43faaba, and should now pass.

app/server/lib/FlexServer.ts
@@ -455,8 +455,9 @@ export class FlexServer implements GristServer {
   }
 
   public getNotifier(): INotifier {
-    // Check that our internal notifier implementation is in place.
-    if (!this.hasNotifier()) { throw new Error('no notifier available'); }
+    // We only warn if we are in a server that doesn't configure notifiers (i.e. not a home
+    // server). But actually having a working notifier isn't required.
+    if (!this._has('notifier')) { throw new Error('no notifier available'); }
     // Expose a wrapper around it that emits actions.
     return this._emitNotifier;
   }