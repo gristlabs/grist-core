0cc150a97109f2b8df4535606f0468b65e35b6c6
Jordi Gutiérrez Hermoso <jordigh@octave.org>
2025-09-26 18:56:39 -0400

    tests: disentangle env setup for Authorizer test
    I need to write a test that does actually use sessions, and this
    environment variable is getting in the way. Let's move it out of the
    way.

--- test/server/lib/Authorizer.ts
+++ test/server/lib/Authorizer.ts
@@ -82,14 +82,12 @@ describe('Authorizer', function() {
   before(async function() {
     this.timeout(5000);
     setUpDB(this);
-    oldEnv = new testUtils.EnvironmentSnapshot();
-    // GRIST_PROXY_AUTH_HEADER now only affects requests directly when GRIST_IGNORE_SESSION is
-    // also set.
-    process.env.GRIST_PROXY_AUTH_HEADER = 'X-email';
-    process.env.GRIST_IGNORE_SESSION = 'true';
     await createInitialDb();
     await activateServer(server, docTools.getDocManager());
     await loadFixtureDocs();
+  });
+  beforeEach(function() {
+    oldEnv = new testUtils.EnvironmentSnapshot();
   });
 
   after(async function() {
@@ -98,6 +96,8 @@ describe('Authorizer', function() {
       await removeConnection();
     });
     assert.lengthOf(messages, 0);
+  });
+  afterEach(function() {
     oldEnv.restore();
   });
 
@@ -292,8 +292,16 @@ describe('Authorizer', function() {
   });
 
   it("can set user via GRIST_PROXY_AUTH_HEADER", async function() {
+    // GRIST_PROXY_AUTH_HEADER now only affects requests directly when GRIST_IGNORE_SESSION is
+    // also set. (These variables are reset by our beforeEach/afterEach hooks.)
+    process.env.GRIST_PROXY_AUTH_HEADER = 'X-email';
+    process.env.GRIST_IGNORE_SESSION = 'true';
+    // We'll need a local setup for this test
+    const localServer = new FlexServer(0, 'test docWorker');
+    await activateServer(localServer, docTools.getDocManager());
+
     // User can access a doc by setting header.
-    const docUrl = `${serverUrl}/o/pr/api/docs/sampledocid_6`;
+    const docUrl = `${localServer.getOwnUrl()}/o/pr/api/docs/sampledocid_6`;
     const resp = await axios.get(docUrl, {
       headers: {'X-email': 'chimpy@getgrist.com'}
     });
@@ -305,7 +313,7 @@ describe('Authorizer', function() {
     }));
 
     // User can access a doc via websocket by setting header.
-    let cli = await openClient(server, 'chimpy@getgrist.com', 'pr', 'X-email');
+    let cli = await openClient(localServer, 'chimpy@getgrist.com', 'pr', 'X-email');
     cli.ignoreTrivialActions();
     assert.equal((await cli.readMessage()).type, 'clientConnect');
     let openDoc = await cli.send("openDoc", "sampledocid_6");
@@ -314,7 +322,7 @@ describe('Authorizer', function() {
     await cli.close();
 
     // Unknown user is denied.
-    cli = await openClient(server, 'notchimpy@getgrist.com', 'pr', 'X-email');
+    cli = await openClient(localServer, 'notchimpy@getgrist.com', 'pr', 'X-email');
     cli.ignoreTrivialActions();
     assert.equal((await cli.readMessage()).type, 'clientConnect');
     openDoc = await cli.send("openDoc", "sampledocid_6");
@@ -322,6 +330,7 @@ describe('Authorizer', function() {
     assert.equal(openDoc.data, undefined);
     assert.match(openDoc.errorCode!, /AUTH_NO_VIEW/);
     await cli.close();
+    await localServer.close();
   });
 });
 
