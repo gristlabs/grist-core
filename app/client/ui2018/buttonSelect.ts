72c8c914da01acd0b0e5ddde5d3bf2a136705237
Emmanuel Pelletier <emmanuel.pelletier@mail.numerique.gouv.fr>
2025-10-07 16:38:51 +0200

    (doclist) make the list/grid view mode buttons accessible
    - use actual button elements to enable keyboard navigation
    - add tooltips on hover to better explain what those icons mean
    - add screen-reader specific labels so that the icons are correctly
    announced in that case, instead of saying just "button"

--- app/client/ui2018/buttonSelect.ts
+++ app/client/ui2018/buttonSelect.ts
@@ -1,6 +1,8 @@
+import {hoverTooltip} from 'app/client/ui/tooltips';
 import {colors, testId, theme, vars} from 'app/client/ui2018/cssVars';
 import {IconName} from 'app/client/ui2018/IconList';
 import {icon} from 'app/client/ui2018/icons';
+import {unstyledButton} from 'app/client/ui2018/unstyled';
 import {isColorDark} from 'app/common/gutil';
 import {components} from 'app/common/ThemePrefs';
 import {dom, DomElementArg, Observable, styled} from 'grainjs';
@@ -9,6 +11,7 @@ import debounce = require('lodash/debounce');
 export interface ISelectorOptionFull<T> {
   value: T;
   label?: string;
+  tooltip?: string;
   icon?: IconName;
 }
 
@@ -113,7 +116,11 @@ export function makeButtonSelect<T>(
     dom.forEach(optionArray, (option: ISelectorOption<T>) => {
       const value = getOptionValue(option);
       const label = getOptionLabel(option);
+      const tooltip = isFullOption(option) && option.tooltip;
+      const screenReaderLabel = !label && tooltip;
       return cssSelectorBtn(
+        tooltip ? hoverTooltip(tooltip) : null,
+        screenReaderLabel ? {"aria-label": screenReaderLabel} : null,
         cssSelectorBtn.cls('-selected', (use) => use(obs) === value),
         dom.on('click', () => onClick(value)),
         isFullOption(option) && option.icon ? icon(option.icon) : null,
@@ -154,11 +161,7 @@ export const cssButtonSelect = styled('div', `
   }
 `);
 
-const cssSelectorBtn = styled('div', `
-  /* Resets */
-  position: relative;
-  outline: none;
-  border-style: none;
+const cssSelectorBtn = styled(unstyledButton, `
   display: flex;
   align-items: center;
   justify-content: center;
