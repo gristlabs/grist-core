gristlabs/grist-core

commit e6692c279332d3e6a168df5d1f304c38da6d0dde

(core) add a checkbox for owner "boss mode"
Summary:
Implement a checkbox that grants owners full access to tables by default, when creating new table/column rules.
 * Checkbox appears above default rules.
 * When set, a rule giving owners full access will be inserted in any new rule set started for tables or columns.
 * The checkbox can be expanded to allow customization of the rules.

https://gristlabs.getgrist.com/doc/check-ins/p/3#a1.s7.r2251.c19

Test Plan: added tests

Reviewers: jarek

Reviewed By: jarek

Subscribers: anaisconce

Differential Revision: https://phab.getgrist.com/D3756

app/server/lib/MinIOExternalStorage.ts
@@ -134,7 +134,13 @@ export class MinIOExternalStorage implements ExternalStorage {
   }
 
   public isFatalError(err: any) {
-    return err.code !== 'NotFound' && err.code !== 'NoSuchKey';
+    // ECONNRESET should not count as fatal:
+    //   https://github.com/aws/aws-sdk-js/pull/3739
+    // Likewise for "We encountered an internal error. Please try again."
+    // These are errors associated with the AWS S3 backend, and which
+    // the AWS S3 SDK would typically handle.
+    return err.code !== 'NotFound' && err.code !== 'NoSuchKey' &&
+      err.code !== 'ECONNRESET' && err.code !== 'InternalError';
   }
 
   public async close() {

app/client/aclui/PermissionsWidget.ts
@@ -15,6 +15,9 @@ import {makeT} from 'app/client/lib/localization';
 // One of the strings 'read', 'update', etc.
 export type PermissionKey = keyof PartialPermissionSet;
 
+// Canonical order of permission bits when rendered in a permissionsWidget.
+const PERMISSION_BIT_ORDER = 'RUCDS';
+
 const t = makeT('PermissionsWidget');
 
 /**
@@ -26,6 +29,7 @@ export function permissionsWidget(
   options: {disabled: boolean, sanityCheck?: (p: PartialPermissionSet) => void},
   ...args: DomElementArg[]
 ) {
+  availableBits = sortBits(availableBits);
   // These are the permission sets available to set via the dropdown.
   const empty: PartialPermissionSet = emptyPermissionSet();
   const allowAll: PartialPermissionSet = makePermissionSet(availableBits, () => 'allow');
@@ -123,6 +127,20 @@ function psetDescription(permissionSet: PartialPermissionSet): string {
   if (allow.length) { parts.push(`Allow ${allow.join(", ")}.`); }
   if (deny.length) { parts.push(`Deny ${deny.join(", ")}.`); }
   return parts.join(' ');
+}
+
+/**
+ * Sort the bits in a standard way for viewing, since they could be in any order
+ * in the underlying rule store. And in fact ACLPermissions.permissionSetToText
+ * uses an order (CRUDS) that is different from how things have been historically
+ * rendered in the UI (RUCDS).
+ */
+function sortBits(bits: PermissionKey[]) {
+  return bits.sort((a, b) => {
+    const aIndex = PERMISSION_BIT_ORDER.indexOf(a.slice(0, 1).toUpperCase());
+    const bIndex = PERMISSION_BIT_ORDER.indexOf(b.slice(0, 1).toUpperCase());
+    return aIndex - bIndex;
+  });
 }
 
 const cssPermissions = styled('div', `

app/client/components/TypeTransform.ts
@@ -20,7 +20,7 @@ import {UserAction} from 'app/common/DocActions';
 import {Computed, dom, fromKo, Observable} from 'grainjs';
 import {makeT} from 'app/client/lib/localization';
 
-const t = makeT('TypeTransformation');
+const t = makeT('TypeTransform');
 
 // To simplify diff (avoid rearranging methods to satisfy private/public order).
 /* eslint-disable @typescript-eslint/member-ordering */

app/common/ACLRuleCollection.ts
@@ -64,6 +64,11 @@ const SPECIAL_RULE_SETS: Record<string, RuleSet> = {
       permissions: parsePermissions('-R'),
       permissionsText: '-R',
     }],
+  },
+  SeedRule: {
+    tableId: SPECIAL_RULES_TABLE_ID,
+    colIds: ['SeedRule'],
+    body: [],
   }
 };
 
@@ -179,9 +184,13 @@ export class ACLRuleCollection {
         const specialType = String(ruleSet.colIds);
         const specialDefault = specialRuleSets.get(specialType);
         if (!specialDefault) {
-          throw new Error(`Invalid rule for ${ruleSet.tableId}:${ruleSet.colIds}`);
+          // Log that we are seeing an invalid rule, but don't fail.
+          // (Historically, older versions of the Grist app will attempt to
+          // open newer documents).
+          options.log.error(`Invalid rule for ${ruleSet.tableId}:${ruleSet.colIds}`);
+        } else {
+          specialRuleSets.set(specialType, {...ruleSet, body: [...ruleSet.body, ...specialDefault.body]});
         }
-        specialRuleSets.set(specialType, {...ruleSet, body: [...ruleSet.body, ...specialDefault.body]});
       }
     }
 

buildtools/generate_translation_keys.js
@@ -18,11 +18,13 @@ const parser = new Parser({
   nsSeparator: null,
 });
 
-async function* walk(dir) {
-  for await (const d of await fs.promises.opendir(dir)) {
-    const entry = path.join(dir, d.name);
-    if (d.isDirectory()) yield* walk(entry);
-    else if (d.isFile()) yield entry;
+async function* walk(dirs) {
+  for (const dir of dirs) {
+    for await (const d of await fs.promises.opendir(dir)) {
+      const entry = path.join(dir, d.name);
+      if (d.isDirectory()) yield* walk([entry]);
+      else if (d.isFile()) yield entry;
+    }
   }
 }
 
@@ -37,6 +39,15 @@ const customHandler = (fileName) => (key, options) => {
     parser.set(keyWithFile, key);
   }
 };
+
+function sort(obj) {
+  if (typeof obj !== "object" || Array.isArray(obj))
+    return obj;
+  const sortedObject = {};
+  const keys = Object.keys(obj).sort();
+  keys.forEach(key => sortedObject[key] = sort(obj[key]));
+  return sortedObject;
+}
 
 const getKeysFromFile = (filePath, fileName) => {
   const content = fs.readFileSync(filePath, "utf-8");
@@ -54,19 +65,20 @@ const getKeysFromFile = (filePath, fileName) => {
   return keys;
 };
 
-async function walkTranslation(dirPath) {
-  for await (const p of walk(dirPath)) {
+async function walkTranslation(dirs) {
+  for await (const p of walk(dirs)) {
     const { name } = path.parse(p);
+    if (p.endsWith('.map')) { continue; }
     getKeysFromFile(p, name);
   }
   const keys = parser.get({ sort: true });
   const newTranslations = _.merge(keys.en.translation, englishKeys);
   await fs.promises.writeFile(
     "static/locales/en.client.json",
-    JSON.stringify(newTranslations, null, 2),
+    JSON.stringify(sort(newTranslations), null, 2),
     "utf-8"
   );
   return keys;
 }
 
-walkTranslation("app/client");
+walkTranslation(["_build/app/client", ...process.argv.slice(2)]);

commit b5a0bea67352c18d030dde0964c7a0fa271b2869

fix(MinioExternalStorage): ECONNRESET and InternalError are fatalErrors (#1874)
## Context

https://github.com/gristlabs/grist-core/commit/e6692c279332d3e6a168df5d1f304c38da6d0dde
mistakenly added 2 error codes to the `isfatalError` method which acts
as a sort of whitelist for MinIO errors that should be safe to ignore.
ECONNRESET and InternalErrors **are** fatal errors.

In production, this meant that when our MinIO server was experiencing
restarts or overall hiccups, the MinioExternalStorage could create a new
document when one already existed, and then push it to MinIO,
overwritting the existing document on the bucket 🥵

## Proposed solution

Remove ECONNRESET and InternalErrors from the list of error codes
MinioExternalStorage should ignore.

## Related issues

Wondering whether the naming of the method lead to confusion in the
first place, should we maybe rename it to `isNotExistingError` or
`isExpectedError` ? I feel like the logic of the code in `head` would
read better (although the comment I added should hopefully fix it).

## Has this been tested?

<!-- Put an `x` in the box that applies: -->

- [ ] 👍 yes, I added tests to the test suite
- [ ] 💭 no, because this PR is a draft and still needs work
- [ ] 🙅 no, because this is not relevant here
- [x] 🙋 no, because I need help

I'm not sure whether I need to add a test here, but apparently the
faulty code was added in order to fix test-related weirdness, so hoping
the tests will pass now.

app/server/lib/MinIOExternalStorage.ts
@@ -82,6 +82,10 @@ export class MinIOExternalStorage implements ExternalStorage {
         ...head.metaData && { metadata: toGristMetadata(head.metaData) },
       };
     } catch (err) {
+      // NotFound and NoSuchKey are "expected" errors when checking for existence of a document
+      // and should return a falsy null.
+      // Other errors like 'ECONNRESET' and 'InternalError' are fatal errors and should be thrown
+      // in order to avoir weird behavior when MinIO is experiencing hiccups.
       if (!this.isFatalError(err)) { return null; }
       throw err;
     }
@@ -185,13 +189,10 @@ export class MinIOExternalStorage implements ExternalStorage {
   }
 
   public isFatalError(err: any) {
-    // ECONNRESET should not count as fatal:
-    //   https://github.com/aws/aws-sdk-js/pull/3739
-    // Likewise for "We encountered an internal error. Please try again."
-    // These are errors associated with the AWS S3 backend, and which
-    // the AWS S3 SDK would typically handle.
-    return err.code !== 'NotFound' && err.code !== 'NoSuchKey' &&
-      err.code !== 'ECONNRESET' && err.code !== 'InternalError';
+    // NotFound and NoSuchKey are "expected" errors when checking for existence of a document
+    // Other errors like 'ECONNRESET' and 'InternalError' were added to the list by mistake
+    // which caused problems like overriding an existing document when MinIO was experiencing hiccups.
+    return err.code !== 'NotFound' && err.code !== 'NoSuchKey';
   }
 
   public async close() {