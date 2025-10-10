19c950966cc4af073da39cf16ec015b1ca509a77
Guillaume Olivrin <guillaume.olivrin@pm.me>
2025-10-10 17:01:25 +0200

    feat(doc-tour): disable HTML rendering in DocTour Markdown - Prevent raw HTML from being rendered within DocTour markdown content - Improves security and consistency of displayed documentation - Updated related tests accordingly

--- app/client/ui/DocTourRenderer.ts
+++ app/client/ui/DocTourRenderer.ts
@@ -1,8 +1,11 @@
 import {constructUrl} from 'app/client/models/gristUrlState';
 import {gristIconLink} from 'app/client/ui2018/links';
 import {marked} from 'marked';
+import escape from "lodash/escape";
 
 export const renderer = new marked.Renderer();
 
 renderer.link = ({href, text}) =>
   gristIconLink(constructUrl(href), text).outerHTML;
+
+renderer.html = ({raw}) => escape(raw);
