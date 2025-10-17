e9675a0036363a922370ca92603317c1472303f1
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2025-10-17 15:02:14 -0400

    deal with bad merge with changes on main

--- app/client/ui/TopBar.ts
+++ app/client/ui/TopBar.ts
@@ -4,6 +4,7 @@ import {loadSearch} from 'app/client/lib/imports';
 import {makeT} from 'app/client/lib/localization';
 import {AppModel, reportError} from 'app/client/models/AppModel';
 import {DocPageModel} from 'app/client/models/DocPageModel';
+import {urlState} from 'app/client/models/gristUrlState';
 import {workspaceName} from 'app/client/models/WorkspaceInfo';
 import {AccountWidget} from 'app/client/ui/AccountWidget';
 import {buildActiveUserList} from 'app/client/ui/ActiveUserList';
