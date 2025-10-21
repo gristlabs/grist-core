b13d69712f7a90e757701cda64dbcc293acbef15
Tristan Robert <tristan.robert.44@gmail.com>
2025-10-21 09:56:23 +0200

    fix: :white_check_mark: fixes attachmentswidget tests

--- test/server/lib/filenamesUtils.ts
+++ test/server/lib/filenamesUtils.ts
@@ -4,12 +4,12 @@ import {assert} from 'chai';
 describe('filenamesUtils', function() {
 
   it('removes non ASCII characters in filename attachment', function() {
-      const content = filenameContentDisposition('attachment', 'sdf% èà.pdf');
-      assert.equal(content, 'attachment; filename="sdf .pdf"');
+      const content = filenameContentDisposition('attachment', 'sdf% èà_.pdf');
+      assert.equal(content, 'attachment; filename="sdf _.pdf"');
   });
 
   it('encodes sanitized filename with starred attachment', function() {
-      const content = filenameStarredContentDisposition('attachment', 'sdf% èà.pdf');
-      assert.equal(content, "attachment; filename*=UTF-8''sdf%20.pdf");
+      const content = filenameStarredContentDisposition('attachment', 'sdf% èà_.pdf');
+      assert.equal(content, "attachment; filename*=UTF-8''sdf%20_.pdf");
     });
 });
