df924dee22c7d7e4def43a165fd1f4459f4c360a
fflorent <florent.fayolle+git@beta.gouv.fr>
2025-11-24 14:33:57 +0100

    Make LinkingBidirectional tests more stable
    Add timeouts to find elements.

--- test/nbrowser/gristUtils.ts
+++ test/nbrowser/gristUtils.ts
@@ -3294,11 +3294,11 @@ export async function removeFilters(save = false) {
 export async function sortAndFilter() {
   const ctrl = {
     async addColumn() {
-      await driver.find('.test-filter-config-add-filter-btn').click();
+      await driver.findWait('.test-filter-config-add-filter-btn', 1000).click();
       return this;
     },
     async clickColumn(col: string) {
-      await driver.findContent(".test-sd-searchable-list-item", col).click();
+      await driver.findContentWait(".test-sd-searchable-list-item", col, 1000).click();
       return this;
     },
     async close() {
@@ -3306,7 +3306,7 @@ export async function sortAndFilter() {
       return this;
     },
     async save() {
-      await driver.find('.test-section-menu-btn-save').click();
+      await driver.findWait('.test-section-menu-btn-save', 1000).click();
       await waitForServer();
       return this;
     },
