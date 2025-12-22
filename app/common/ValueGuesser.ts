e1d293f7a495a3ea6145b6dcfd933053224611e9
fflorent <florent.fayolle+git@beta.gouv.fr>
2025-12-22 14:20:27 +0100

    Eslint: Fix @stylistic/indent-binary-ops

--- app/common/ValueGuesser.ts
+++ app/common/ValueGuesser.ts
@@ -189,11 +189,11 @@ export function guessColInfo(
         NumberParse.fromSettings(docSettings).guessOptions(values),
       )
         .guess(values, docSettings) ||
-      new DateGuesser(guessDateFormat(values, timezone), timezone)
+        new DateGuesser(guessDateFormat(values, timezone), timezone)
         .guess(values, docSettings) ||
     // Don't return the same values back if there's no conversion to be done,
     // as they have to be serialized and transferred over a pipe to Python.
-      {colInfo: {type: 'Text'}}
+        {colInfo: {type: 'Text'}}
   );
 }
 
