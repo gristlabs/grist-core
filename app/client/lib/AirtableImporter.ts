124ab55f55df5c9201a64495ac1cd959e3aa6b80
Spoffy <contact@spoffy.net>
2026-01-19 21:48:31 +0000

    Improves logs for airtable import exemplar

--- app/client/lib/AirtableImporter.ts
+++ app/client/lib/AirtableImporter.ts
@@ -7,19 +7,7 @@ import {
   ImportSchemaTransformParams,
   transformImportSchema, validateImportSchema,
 } from "app/common/DocSchemaImport";
-
-/*
-Importer will:
-  - Optionally create a new doc - wipe existing tables
-  - Fetch details of base (_api.getBaseSchema) - display them to the user
-  - User specifies transforms to be applied. Each transform runs a new validation
-  - User finally approves, executes.
-  - Creates schema, reports any warnings.
-  - Specify transforms to be applied
-  - Apply transforms
-  - Get the schema from the given service (or be given it)
-  - Apply schema to the doc?
- */
+import { UserAPI } from "app/common/UserAPI";
 
 const G = getBrowserGlobals("window");
 
@@ -27,16 +15,25 @@ export function addAirtableMigrationBrowserGlobal() {
   G.window.runAirtableMigration = runAirtableMigration;
 }
 
+/**
+ * Exemplar function for importing an airtable base into a Grist document.
+ * @param {string} apiKey - Airtable Personal Access Token
+ * @param {string} baseId - ID of the Airtable base to import
+ * @param {ImportSchemaTransformParams} transformations - Transformations to apply to the schema
+ * @param {string} [existingDocId] - If defined, imports tables into this existing Grist doc.
+ * @returns {Promise<void>}
+ */
 export async function runAirtableMigration(
   apiKey: string,
   baseId: string,
   transformations?: ImportSchemaTransformParams,
-) {
+  existingDocId?: string,
+): Promise<void> {
   const api = new AirtableAPI({ apiKey });
 
   const bases = await api.listBases();
 
-  console.log("Bases");
+  console.log("All available Airtable bases:");
   console.log(bases);
 
   const baseToUse = bases.find(base => base.id === baseId);
@@ -47,50 +44,57 @@ export async function runAirtableMigration(
   // We can try using a custom DocComm inside a FlowRunner here if needed.
   const userApi = window.gristApp?.topAppModel?.api;
   if (!userApi) {
-    throw new Error("No user API available");
+    throw new Error("No user API available - is this being run outside of the browser?");
   }
 
   const baseSchema = await api.getBaseSchema(baseToUse.id);
 
-  console.log("Base schema");
+  console.log("Retrieved schema for the selected Airtable base:");
   console.log(baseSchema);
 
-  const workspaces = await userApi.getOrgWorkspaces("current");
-  const docId = await userApi.newDoc({ name: baseToUse.name }, workspaces[0].id);
-  const docApi = userApi.getDocAPI(docId);
+  const docApi = existingDocId ?
+    userApi.getDocAPI(existingDocId) : userApi.getDocAPI(await createDoc(userApi, baseToUse.name));
 
   const existingDocSchema = await getExistingDocSchema(docApi);
-  console.log("Existing doc schema");
+  console.log("Schema for the existing Grist document:");
   console.log(existingDocSchema);
   const initialTables = existingDocSchema.tables.map(table => table.id);
 
   const importSchema = gristDocSchemaFromAirtableSchema(baseSchema);
 
-  console.log("Import schema");
+  console.log("Generated Grist schema from the Airtable base:");
   console.log(importSchema);
 
-  console.log("Import schema validation warnings");
-  console.log(validateImportSchema(importSchema));
+  console.log("Validation warnings for the generated Grist schema:");
+  console.warn(validateImportSchema(importSchema));
 
   const transformedSchema = transformImportSchema(importSchema, transformations ?? {}, existingDocSchema);
 
-  console.log("Transformed import schema");
+  console.log("Generated Grist schema after transformations are applied:");
   console.log(transformedSchema.schema);
 
-  console.log("Transformation warnings");
+  console.log("Warnings that occurred during the transformation:");
   console.log(transformedSchema.warnings);
 
-  console.log("Transformed schema validation warnings");
+  console.log("Validation warnings for the transformed Grist schema:");
   console.log(validateImportSchema(transformedSchema.schema));
 
   const docSchemaCreator = new DocSchemaImportTool(actions => docApi.applyUserActions((actions)));
 
   const { warnings: creationWarnings } = await docSchemaCreator.createTablesFromSchema(transformedSchema.schema);
-  console.log("Creation warnings");
+  console.log("Warnings that occurred when applying the schema to the document:");
   console.log(creationWarnings);
 
-  await docSchemaCreator.removeTables(initialTables);
+  // Only remove the initial tables if the Grist document was newly created.
+  if (!existingDocId) {
+    await docSchemaCreator.removeTables(initialTables);
+  }
 
-  console.log("Final real doc schema");
+  console.log("Final schema of the Grist document:");
   console.log(await getExistingDocSchema(docApi));
+}
+
+async function createDoc(userApi: UserAPI, name: string) {
+  const workspaces = await userApi.getOrgWorkspaces("current");
+  return await userApi.newDoc({ name }, workspaces[0].id);
 }
