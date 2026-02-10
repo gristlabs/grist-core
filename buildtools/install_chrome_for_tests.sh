c19c39aa47d88faac576931cd745127d06fc380b
fflorent <florent.fayolle+git@beta.gouv.fr>
2026-02-10 17:04:11 +0100

    Use again the official Google Chrome URL

--- buildtools/install_chrome_for_tests.sh
+++ buildtools/install_chrome_for_tests.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-CHROME_VERSION=144.0.7559.132-1
+CHROME_VERSION="144.0.7559.132-1"
 
 if [[ "$1" != "-y" ]]; then
   echo "Usage: $0 -y"
@@ -18,10 +18,7 @@ if [[ "$(uname -m)" != "x86_64" ]]; then
   exit 1
 fi
 
-# Google's official repo no longer keeps old versions, using UChicago mirror instead.
-# Original URL (may work again in future):
-#   https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_VERSION}_amd64.deb
-curl -sS -o /tmp/chrome.deb https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_VERSION}_amd64.deb \
+curl -sS -o /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_VERSION}_amd64.deb \
   && sudo apt-get install --allow-downgrades -y /tmp/chrome.deb \
   && rm /tmp/chrome.deb \
   && sudo rm /usr/bin/chromedriver \
