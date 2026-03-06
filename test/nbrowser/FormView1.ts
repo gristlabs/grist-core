11758cb00f15ccbe4090e4784ef96c517a9b13dc
fflorent <florent.fayolle+git@beta.gouv.fr>
2026-03-06 17:01:53 +0100

    Fix FormView1 tests

--- test/nbrowser/FormView1.ts
+++ test/nbrowser/FormView1.ts
@@ -1449,6 +1449,7 @@ describe("FormView1", function() {
     it("cutting works", async function() {
       const revert = await gu.begin();
       await question("A").click();
+      await gu.waitAppFocus();
       // Send copy command.
       await clipboard.lockAndPerform(async (cb) => {
         await cb.cut();
@@ -1470,6 +1471,7 @@ describe("FormView1", function() {
       await clickMenu("Paragraph");
       await gu.waitForServer();
       await element("Paragraph", 5).click();
+      await gu.waitAppFocus();
       await clipboard.lockAndPerform(async (cb) => {
         await cb.cut();
         // Go over A and paste there.
