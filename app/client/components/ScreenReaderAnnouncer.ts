4550eda95b5347961867bc5db17a5d3235a6a93c
Emmanuel Pelletier <emmanuel.pelletier@mail.numerique.gouv.fr>
2026-03-25 14:52:27 +0100

    screen reader Edge bug: change how we "clean up" the announcer
    In Edge+NVDA, as soon as we reached the limit of 10 children in the
    screen reader announcer div, any announcement would vocalize the whole
    `#screen-reader-announcer` instead of only the added child `div`.
    
    This was due to the fact that we removed children progressively (we add
    a child, we notice we have 11 children, we remove the first child to
    always have 10 children max). For some reason Edge+NVDA didn't like
    that.
    
    Now we regularly clean the #screen-reader-announcer instead. Manual
    tests shown that this doesn't trigger any superfluous vocalization.

--- app/client/components/ScreenReaderAnnouncer.ts
+++ app/client/components/ScreenReaderAnnouncer.ts
@@ -10,20 +10,30 @@ const t = makeT("ScreenReaderAnnouncer");
 export class ScreenReaderAnnouncer extends Disposable {
   private readonly _container: HTMLDivElement;
   private _debouncedByKey: Record<string, ReturnType<typeof debounce<(announcements: string[]) => void>>> = {};
+  private _debouncedCleanup: ReturnType<typeof debounce<() => void>>;
 
   constructor() {
     super();
+
     this._container = visuallyHidden({
       "id": "screen-reader-announcer",
+      "role": "region",
       "aria-live": "polite",
       "aria-atomic": "false",
+      // This is not supported by all screen readers but it helps with some of them.
+      "aria-relevant": "additions",
     });
+
+    this._debouncedCleanup = debounce(() => this._cleanup(), 1000);
+
     document.body.appendChild(this._container);
+
     this.onDispose(() => {
       dom.domDispose(this._container);
       this._container.remove();
       Object.values(this._debouncedByKey).forEach(debouncedFunction => debouncedFunction.cancel());
       this._debouncedByKey = {};
+      this._debouncedCleanup.cancel();
     });
   }
 
@@ -36,7 +46,7 @@ export class ScreenReaderAnnouncer extends Disposable {
    */
   public announce(announcements: string | string[], key: string = "default") {
     if (!this._debouncedByKey[key]) {
-      this._debouncedByKey[key] = debounce((messages: string[]) => this._announce(messages), 100);
+      this._debouncedByKey[key] = debounce((messages: string[]) => this._announce(messages), 350);
     }
     this._debouncedByKey[key](Array.isArray(announcements) ? announcements : [announcements]);
   }
@@ -58,19 +68,38 @@ export class ScreenReaderAnnouncer extends Disposable {
     if (!announcements.length) {
       return;
     }
+
+    // Cancel the potential ongoing cleanup process so that it doesn't interfere with the new announcement.
+    this._debouncedCleanup.cancel();
+
     // Since aria-live regions strip HTML semantics, we play with punctuation to help SRs make pauses at the right
-    // time when announcing multiple things in a row:
-    // - we add a comma between each "part of announcement"
-    // - we add a period after each "pack of announcements" if none is present
-    let toAnnounce = announcements.join(", ");
-    if (!toAnnounce.endsWith(".")) {
-      toAnnounce += ".";
+    // time when announcing multiple things in a row. We add a comma every time we want the SR to make a brief pause.
+    // Be careful if you change this. The "," char was picked after careful testing across different screen readers.
+    let toAnnounce = announcements.map(a => a.trim()).join(", ");
+    const endCharacters = ".!,:?;";
+    if (!endCharacters.includes(toAnnounce.at(-1)!)) {
+      toAnnounce += ",";
     }
     this._container.appendChild(dom("div", toAnnounce));
 
-    // Make sure the DOM doesn't get too big
-    while (this._container.children.length > 10) {
-      this._container.removeChild(this._container.firstChild!);
+    // Schedule a cleanup process that will happen after a tiny pause if no new announcement is made.
+    this._debouncedCleanup();
+  }
+
+  /**
+   * Remove the content of the announcer div if it has more than 10 children.
+   *
+   * This is meant to be called regularly, to prevent the DOM from getting too big.
+   */
+  private _cleanup() {
+    // We could just cleanup directly instead of checking for children count first, but it's pretty handy when
+    // developing to have a recent history of announcements.
+    if (this._container.children.length <= 10) {
+      return;
     }
+    // The "clean up" is done through *removing all children*, and not another way, on purpose. Changing content of an
+    // `aria-live` region is rather sensitive, and doing it wrong can lead to major announcement issues from some SRs.
+    // You should test the behavior manually with NVDA, JAWS and VoiceOver if you change this.
+    this._container.innerHTML = "";
   }
 }
