e2a8f1880a0288a42241a0a88ed5210a945bb4a0
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2026-04-15 15:12:20 -0400

    split MultiColumn further

--- test/nbrowser/MultiColumn2.ts
+++ test/nbrowser/MultiColumn2.ts
@@ -13,8 +13,6 @@ import {
   removeColumn,
   selectColumns,
   setupMultiColumnDoc,
-  slider,
-  sliderDisabled,
   testAlignment,
   testChoices,
   testSingleWrapping,
@@ -238,71 +236,6 @@ describe("MultiColumn2", function() {
         } else {
           await selectColumns("Left", "Right");
           assert.isTrue(await widgetTypeDisabled());
-        }
-      });
-    }
-  });
-
-  // Any and Text column are identical in terms of formatting.
-  for (const left of ["Text", "Any"]) {
-    describe(`test for ${left} column`, function() {
-      beforeEach(async () => {
-        await gu.addColumn("Left", left);
-      });
-      afterEach(async function() {
-        if (this.currentTest?.state === "passed") {
-          await removeColumn("Left");
-          await removeColumn("Right");
-        }
-      });
-      for (const right of types) {
-        it(`should work with ${right} column`, async function() {
-          await gu.addColumn("Right", right);
-          await selectColumns("Left", "Right");
-          if (left === "Text") {
-            if (right === "Text") {
-              assert.isFalse(await widgetTypeDisabled());
-            } else {
-              assert.isTrue(await widgetTypeDisabled());
-            }
-          }
-          await commonTestsForAny(right);
-        });
-      }
-    });
-  }
-
-  describe(`test for Attachment column`, function() {
-    beforeEach(async () => {
-      await gu.addColumn("Left", "Attachment");
-    });
-    afterEach(async function() {
-      if (this.currentTest?.state === "passed") {
-        await removeColumn("Left");
-        await removeColumn("Right");
-      }
-    });
-    // Test for types that matter (have different set of defaults).
-    for (const right of ["Any", "Attachment"]) {
-      it(`should work with ${right} column`, async function() {
-        await gu.addColumn("Right", right);
-        await selectColumns("Left", "Right");
-        if (right !== "Attachment") {
-          assert.isTrue(await sliderDisabled());
-        } else {
-          assert.isFalse(await sliderDisabled());
-          // Test it works as expected
-          await slider(16); // min value
-          assert.equal(await slider(), 16);
-          await selectColumns("Left");
-          assert.equal(await slider(), 16);
-          await selectColumns("Right");
-          assert.equal(await slider(), 16);
-          // Set max for Right column, left still has minium
-          await slider(96); // max value
-          await selectColumns("Left", "Right");
-          // When mixed, slider is in between.
-          assert.equal(await slider(), (96 - 16) / 2 + 16);
         }
       });
     }
