ac0d97bbbc8541a4749941210672cba96827388c
wvengen <dev-grist@willem.engen.nl>
2026-04-28 20:09:15 +0200

    Changes after code-review

--- sandbox/pyodide/setup.sh
+++ sandbox/pyodide/setup.sh
@@ -9,14 +9,14 @@ echo "## Get pyodide node package"
 source env.sh
 
 # make sure we have the right version of pyodide defined
-CUR_PYODIDE_VERSION=`node -p 'require("./worker/package.json").dependencies.pyodide'`
+CUR_PYODIDE_VERSION=`node -p 'JSON.parse(require("fs").readFileSync("./worker/package.json")).dependencies.pyodide'`
 if [[ "${CUR_PYODIDE_VERSION}" != "${PYODIDE_VERSION}" ]]; then
-  echo "Worker pyodide version ${CUR_PYODIDE_VERSION} != ${PYODIDE_VERSION}, aborting."
+  echo "Please ensure the versions of Pyodide in package.json (${CUR_PYODIDE_VERSION}) and env.sh (${PYODIDE_VERSION}) match."
   exit 1
 fi
 
 # make sure we have the right version of pyodide installed (in case package.json was updated after install)
-INST_PYODIDE_VERSION=`node -p 'require("./worker/node_modules/pyodide/package.json").version' 2>/dev/null || true`
+INST_PYODIDE_VERSION=`node -p 'JSON.parse(require("fs").readFileSync("./worker/node_modules/pyodide/package.json")).version' 2>/dev/null || true`
 if [[ "${INST_PYODIDE_VERSION}" != "${PYODIDE_VERSION}" ]]; then
   yarn install --frozen-lockfile --cwd worker
 fi
