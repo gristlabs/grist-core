56a8e96e64a4ebda30f24848f850b9d2f2bff40d
Emmanuel Pelletier <emmanuel.pelletier@mail.numerique.gouv.fr>
2026-04-29 14:22:23 +0200

    Make sure the default locale comes from GRIST_DEFAULT_LOCALE
    When we created a document and we couldn't deduce the locale from the
    request, it always set the locale to en-US. But we have a
    GRIST_DEFAULT_LOCALE variable.
    
    Now the code that always defaulted to en-US defaults to
    GRIST_DEFAULT_LOCALE instead.
    
    We also put a warning in the server logs when we notice that the env var
    doesn't match a locale we expect. It's useful to easily make people
    understand that we expect something like `en-US` and not `en`.

--- app/server/lib/ServerLocale.ts
+++ app/server/lib/ServerLocale.ts
@@ -1,14 +1,27 @@
+import { localeCodes } from "app/common/LocaleCodes";
 import { locales } from "app/common/Locales";
+import log from "app/server/lib/log";
 
 import { IncomingMessage } from "http";
 
 import { parse as languageParser } from "accept-language-parser";
 
+const fallbackLocale = "en-US";
+
+export function getDefautLocale() {
+  let locale = process.env.GRIST_DEFAULT_LOCALE;
+  if (locale && !localeCodes.includes(locale)) {
+    log.warn(`Invalid GRIST_DEFAULT_LOCALE, falling back to ${fallbackLocale}. Check app/common/LocaleCodes.ts for supported locales.`);
+    locale = fallbackLocale;
+  }
+  return locale ?? fallbackLocale;
+}
+
 /**
  * Returns the locale from a request, falling back to `defaultLocale`
  * if unable to determine the locale.
  */
-export function localeFromRequest(req: IncomingMessage, defaultLocale: string = "en-US") {
+export function localeFromRequest(req: IncomingMessage, defaultLocale: string = getDefautLocale()) {
   const language = languageParser(req.headers["accept-language"]!)[0];
   if (!language) { return defaultLocale; }
 
