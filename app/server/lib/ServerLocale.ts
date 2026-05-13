1e0459e67a9003a8c7d79b63db1c4de03bd5960d
Emmanuel Pelletier <emmanuel.pelletier@mail.numerique.gouv.fr>
2026-05-13 11:44:54 +0200

    woopsy typo: getDefautLocale -> getDefaultLocale

--- app/server/lib/ServerLocale.ts
+++ app/server/lib/ServerLocale.ts
@@ -7,7 +7,7 @@ import { parse as languageParser } from "accept-language-parser";
 
 const fallbackLocale = "en-US";
 
-export function getDefautLocale() {
+export function getDefaultLocale() {
   let locale = process.env.GRIST_DEFAULT_LOCALE;
   if (locale && !localeCodes.includes(locale)) {
     log.warn(`Invalid GRIST_DEFAULT_LOCALE, falling back to ${fallbackLocale}. Check app/common/LocaleCodes.ts for supported locales.`);
@@ -20,7 +20,7 @@ export function getDefautLocale() {
  * Returns the locale from a request, falling back to `defaultLocale`
  * if unable to determine the locale.
  */
-export function localeFromRequest(req: IncomingMessage, defaultLocale: string = getDefautLocale()) {
+export function localeFromRequest(req: IncomingMessage, defaultLocale: string = getDefaultLocale()) {
   const languages = languageParser(req.headers["accept-language"]!);
   const match = languages.find(l => l.code && l.region && localeCodes.includes(`${l.code}-${l.region}`));
   return match ? `${match.code}-${match.region}` : defaultLocale;
