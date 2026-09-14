202995f77ddb4676ba8187ce9088a7ad1e345840
Jonathan Perret <jonathan.perret@beta.gouv.fr>
2026-09-14 16:21:04 +0200

    Remove needless whitespace change

--- app/server/lib/ActiveDoc.ts
+++ app/server/lib/ActiveDoc.ts
@@ -2684,7 +2684,6 @@ export class ActiveDoc extends EventEmitter {
       } catch (err) {
         this._log.error(docSession, "failed to shutdown some resources", err);
       }
-
       // No timeout on this callback: if it hangs, it will make the document unusable.
       await this._afterShutdownCallback?.();
     } finally {
